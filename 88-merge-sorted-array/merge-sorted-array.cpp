65c39dcdedcfc94e17cd2579dca4748cdf4c7459
Shashank Ranjan <92708818+Cheekybaby@users.noreply.github.com>
2023-12-27 23:17:21 +0530

    Time: 2 ms (48.93%) | Memory: 9.5 MB (17.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0, j=0, k=0;
-        vector<int> nums(m+n);
-        while (i<m && j<n){
-            if (nums1[i]<=nums2[j]){
-                nums[k] = nums1[i];
-                k+=1;
-                i+=1;
-            } else if (nums1[i]>nums2[j]){
-                nums[k] = nums2[j];
-                k+=1;
-                j+=1;
+        int pos = n+m-1;
+        int i = m-1, j = n-1;
+        while (pos>=0){
+            if (j<0){
+                nums1[pos] = nums1[i];
+                i-=1;
+                pos-=1;
+                continue;
             }
-        }
-
-        while (i<m){
-            nums[k] = nums1[i];
-            i+=1;
-            k+=1;
-        }
-        while (j<n){
-            nums[k] = nums2[j];
-            j+=1;
-            k+=1;
-        }
-
-        for (int l=0; l<m+n; l+=1){
-            nums1[l] = nums[l];
+            if (i<0){
+                nums1[pos] = nums2[j];
+                j-=1;
+                pos-=1;
+                continue;
+            }
+            if (nums1[i]>=nums2[j]){
+                nums1[pos] = nums1[i];
+                i-=1;
+            } else {
+                nums1[pos] = nums2[j];
+                j-=1;
+            }
+            pos-=1;
         }
     }
 };

85a21a40bb0cea87bb77d146e4a6f0cb429ba625
Shashank Ranjan <92708818+Cheekybaby@users.noreply.github.com>
2023-12-29 13:44:48 +0530

    Time: 17 ms (26.79%) | Memory: 7.5 MB (96.50%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int k = s1.length();
-        if (s2.length() < k){
+        if (s2.length()< k){
             return false;
         }
-        vector<int> hashmpp(26,0);
-        vector<int> window(26,0);
+        vector<int> window(26);
+        vector<int> hashmapp(26);
 
-        for (int i=0; i<k; i++){
-            hashmpp[s1[i]-'a']++;
-            window[s2[i]-'a']++;
+        for (int i=0; i<k; i+=1){
+            window[s2[i] - 'a']+=1;
+            hashmapp[s1[i]-'a']+=1;
         }
-        
-        if (hashmpp == window){
+
+        if (window == hashmapp){
             return true;
         }
 
-        for (int i=k; i<s2.length(); i++){
-            window[s2[i]-'a']++;
-            window[s2[i-k]-'a']--;
+        for (int i=k; i<s2.length(); i+=1){
+            window[s2[i] - 'a']+=1;
+            window[s2[i-k] - 'a']-=1;
 
-            if (window == hashmpp){
+            if (window == hashmapp){
                 return true;
             }
         }

fc476934d7483e76923f4f53cf90cb10ce475ee2
Shashank Ranjan <92708818+Cheekybaby@users.noreply.github.com>
2024-02-04 20:34:01 +0530

    Time: 210 ms (68.98%) | Memory: 113.4 MB (18.75%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,56 @@
+// class Solution {
+// public:
+//     bool sub(vector<int>nums,int target,int ind,vector<vector<int>>&dp){
+//         if(target==0) return true;
+//         if(ind==0) return (nums[0]==target);
+//         if(dp[target][ind]!=-1) return dp[target][ind];
+//         bool notTake=sub(nums,target,ind-1,dp);
+//         bool Take=false;
+//         if(targetnums[ind]) Take=sub(nums,target-nums[ind],ind-1,dp);
+//         return dp[target][ind]=(notTake|| Take);
+//     }
+//     bool canPartition(vector<int>& nums) {
+//         int n=nums.size();
+//         int totalSum=0;
+
+//         for(auto it:nums) totalSum+=it;
+//         if(totalSum%2) return false;
+//         int target=totalSum/2;
+//         vector<vector<int>>dp(target,vector<int>(n+1,-1));
+//         if(sub(nums,target,n,dp)) return true;
+//         return false;
+
+         
+//     }
+// };
+
+class Solution {
+public:
+    bool sub(vector<int>& nums, int target, int ind, vector<vector<int>>& dp) {
+        if (target == 0) return true;
+        if (ind == 0) return (nums[0] == target);
+        if (dp[target][ind] != -1) return dp[target][ind];
+        
+        bool notTake = sub(nums, target, ind - 1, dp);
+        bool take = false;
+        if (target >= nums[ind]) {
+            take = sub(nums, target - nums[ind], ind - 1, dp);
+        }
+        
+        return dp[target][ind] = (notTake || take);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = 0;
+
+        for (auto it : nums) totalSum += it;
+        if (totalSum % 2) return false;
+
+        int target = totalSum / 2;
+        vector<vector<int>> dp(target + 1, vector<int>(n + 1, -1));
+
+        if (sub(nums, target, n - 1, dp)) return true;
+        return false;
+    }
+};

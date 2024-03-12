dfd3ca28d46af5be052e86569ed2cf29930d8085
Shashank Ranjan <92708818+Cheekybaby@users.noreply.github.com>
2024-03-13 00:14:52 +0530

    Time: 10 ms (35.49%) | Memory: 15.1 MB (16.76%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -11,30 +11,37 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        ListNode* dummy = new ListNode(0);
+        int prefixSum = 0;
+        ListNode *dummy = new ListNode(0);
         dummy->next = head;
-        int prefix_sum = 0;
-        std::unordered_map<int, ListNode*> prefix_sums;
-        prefix_sums[0] = dummy;
-        ListNode* current = head;
+        unordered_map<int, ListNode*> mpp;
+        mpp[0] = dummy;
 
-        while (current) {
-            prefix_sum += current->val;
-            if (prefix_sums.find(prefix_sum) != prefix_sums.end()) {
-                ListNode* to_delete = prefix_sums[prefix_sum]->next;
-                int temp_sum = prefix_sum + to_delete->val;
-                while (to_delete != current) {
-                    prefix_sums.erase(temp_sum);
-                    to_delete = to_delete->next;
-                    temp_sum += to_delete->val;
+        while (head!=nullptr){
+            prefixSum+=head->val;
+
+            if (mpp.find(prefixSum)!=mpp.end()){
+                ListNode *start = mpp[prefixSum];
+                ListNode *temp = start;
+
+                int pSum = prefixSum;
+
+                while (temp!=head){
+                    temp = temp->next;
+                    pSum+=temp->val;
+
+                    if (temp!=head){
+                        mpp.erase(pSum);
+                    }
                 }
-                prefix_sums[prefix_sum]->next = current->next;
-            } else {
-                prefix_sums[prefix_sum] = current;
-            }
-            current = current->next;
-        }
+                start->next = head->next;
 
+            } else {
+                mpp[prefixSum] = head;
+            }
+
+            head = head->next;
+        }
         return dummy->next;
     }
 };

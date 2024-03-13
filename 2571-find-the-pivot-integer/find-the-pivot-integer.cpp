e24878c53be5ef95d78128648603652fbfabd114
Shashank Ranjan <92708818+Cheekybaby@users.noreply.github.com>
2024-03-13 09:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int sum = n*(n+1)/2;
-        // Binary Search
-        int l=1, r=n;
-        while (l<=r){
-            int mid = l+(r-l)/2;
-            int suml = mid*(mid+1)/2;
-            int sumr = sum - suml + mid;
-            if (suml == sumr){
-                return mid;
-            } else if (suml < sumr){
-                l = mid+1;
-            } else {
-                r = mid-1;
+        int temp = 0, i=1;
+        while (temp<sum){
+            temp+=i;
+            if (temp == (sum-temp+i)){
+                return i;
             }
+            i+=1;
         }
         return -1;
     }

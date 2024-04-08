dafb0b3779471c4ac8dfde8a94f82626fd325c10
Shashank Ranjan <shashankranjan07@gmail.com>
2024-04-08 14:29:00 +0530

    Time: 5 ms (31.29%) | Memory: 10.7 MB (79.46%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        queue<int> q_student;
-        stack<int> st_sandwich;
+        int square = 0, triangle = 0;
         for (int i=0; i<students.size(); i+=1){
-            q_student.push(students[i]);
-        }
-        for (int i=sandwiches.size()-1; i>=0; i-=1){
-            st_sandwich.push(sandwiches[i]);
-        }
-        int count = 0;
-        while (!q_student.empty() && count < q_student.size()){
-            if (q_student.front() == st_sandwich.top()){
-                q_student.pop();
-                st_sandwich.pop();
-                count = 0;
+            if (students[i] == 1){
+                square+=1;
             } else {
-                q_student.push(q_student.front());
-                q_student.pop();
-                count+=1;
+                triangle+=1;
             }
         }
 
-        return q_student.size();
+        for (int i=0; i<sandwiches.size(); i+=1){
+            if (sandwiches[i] == 1){
+                if (square > 0){
+                    square-=1;
+                } else {
+                    break;
+                }
+            } else {
+                if (triangle > 0){
+                    triangle-=1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return square+triangle;
     }
 };

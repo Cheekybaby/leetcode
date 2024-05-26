39a2c67d8454a592489cc5fd3dc68aa8050b65ed
Shashank Ranjan <shashankranjan07@gmail.com>
2024-05-26 20:43:29 +0530

    Time: 7 ms (82.05%) | Memory: 17.9 MB (91.48%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int binarySearch(vector<int> &v, int left, int right, int x){
-        while (left<=right){
-            int mid = left + (right-left)/2;
-            if (v[mid] == x) return mid;
-            else if (v[mid]>x) right = mid-1;
-            else left = mid+1;
-        }
-
-        return -1;
-    }
     vector<int> twoSum(vector<int>& numbers, int target) {
-        for (int i=0; i<numbers.size(); i+=1){
-            int tofind = target - numbers[i];
-            if (binarySearch(numbers, i+1, numbers.size()-1, tofind) != -1){
-                return {i+1, binarySearch(numbers, i+1, numbers.size()-1, tofind)+1};
+        int i=0, j=numbers.size()-1;
+        while (i<j){
+            if(numbers[i]+numbers[j] == target){
+                return {i+1, j+1};
+            } else if (numbers[i]+numbers[j] > target){
+                j-=1;
+            } else {
+                i+=1;
             }
         }
         return {-1,-1};

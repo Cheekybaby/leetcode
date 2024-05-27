1290ee0a7b57fd2bdb0f2efbab5194e988158996
Shashank Ranjan <shashankranjan07@gmail.com>
2024-05-27 14:23:44 +0530

    Time: 143 ms (63.47%) | Memory: 125.8 MB (80.38%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        for (int i=1; i<nums.size()-1; i+=1){
-            if ((nums[i-1]<nums[i] && nums[i]<nums[i+1]) || (nums[i-1]>nums[i] && nums[i]>nums[i+1])){
+        int i = 1;
+        while (i<nums.size()-1){
+            if ((nums[i]>nums[i-1] && nums[i]<nums[i+1]) || (nums[i]<nums[i-1] && nums[i]>nums[i+1])){
                 swap(nums[i], nums[i+1]);
             }
+            i+=1;
         }
 
         return nums;

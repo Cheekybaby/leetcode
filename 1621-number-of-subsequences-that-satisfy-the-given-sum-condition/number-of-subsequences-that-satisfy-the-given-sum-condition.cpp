f2401381bd6859860f020794c92faacf89781bd7
Shashank Ranjan <shashankranjan07@gmail.com>
2024-05-27 13:47:30 +0530

    Time: 91 ms (60.16%) | Memory: 51.2 MB (93.42%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,23 +1,30 @@
+using ll = long long;
 class Solution {
 public:
-    int numSubseq(vector<int>& nums, int target) {
-        const int mod = 1e9+7;
-        sort(nums.begin(), nums.end());
-
-        vector<int> power(nums.size()+1, 1);
-
-        for (int i=1; i<=nums.size(); i+=1){
-            power[i] = (power[i-1]<<1)%mod;
+    ll power(ll a, ll b, int mod){
+        ll ans = 1;
+        while (b!=0){
+            if (b%2 == 1){
+                ans = (ans*a)%mod;
+            }
+            a = (a*a)%mod;
+            b/=2;
         }
 
-        int left = 0, right = nums.size()-1;
+        return ans;
+    }
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int i=0, j=nums.size()-1;
         int ans = 0;
-        while (left<=right){
-            if (nums[left]+nums[right]<=target){
-                ans = (ans + power[right-left])%mod;
-                left+=1;
-            } else {
-                right-=1;
+        const int mod = 1e9+7;
+
+        while (i<=j){
+            if (nums[i]+nums[j]>target){
+                j-=1;
+            } else{
+                ans = (ans + (power(2, j-i, mod)))%mod;
+                i+=1;
             }
         }
 

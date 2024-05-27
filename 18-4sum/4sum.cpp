025bc7a4abeea2cdfbd8597d6ac3161b6e69ca2c
Shashank Ranjan <shashankranjan07@gmail.com>
2024-05-27 13:16:27 +0530

    Time: 169 ms (23.55%) | Memory: 52 MB (18.36%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -9,16 +9,16 @@ public:
         for(int i=0; i<n-3; i+=1){
             for (int j=i+1; j<n-2; j+=1){
                 ll newtarget = ll(target) - ll(nums[i]) - ll(nums[j]);
-                int low = j+1, high = n-1;
-                while (low<high){
-                    if (nums[low]+nums[high]<newtarget){
-                        low+=1;
-                    } else if (nums[low]+nums[high]>newtarget){
-                        high-=1;
+                int k = j+1, l = n-1;
+                while (k<l){
+                    if (nums[k]+nums[l]<newtarget){
+                        k+=1;
+                    } else if (nums[k]+nums[l]>newtarget){
+                        l-=1;
                     } else {
-                        st.insert({nums[i], nums[j], nums[low], nums[high]});
-                        low+=1;
-                        high-=1;
+                        st.insert({nums[i], nums[j], nums[k], nums[l]});
+                        k+=1;
+                        l-=1;
                     }
                 }
             }

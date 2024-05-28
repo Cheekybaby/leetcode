53d92184cee8763d45ab86a328cbc886d85c9871
Shashank Ranjan <shashankranjan07@gmail.com>
2024-05-28 16:55:01 +0530

    Time: 13 ms (55.36%) | Memory: 9.9 MB (88.11%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
-        int k = p.length();
-        if (k>s.length()){
-            return {};
+        if (p.length() > s.length()) return ans;
+        vector<int> ph(26,0), window(26,0);
+        for (int i=0; i<p.length(); i+=1){
+            ph[p[i]-'a']+=1;
+            window[s[i]-'a']+=1;
         }
 
-        vector<int> phash(26, 0);
-        vector<int> window(26, 0);
+        if (ph == window) ans.push_back(0);
 
-        for (int i=0; i<k; i++){
-            phash[p[i]-'a']++;
-            window[s[i]-'a']++;
-        }
-
-        if (phash == window){
-            ans.push_back(0);
-        }
-
-        for (int i=k; i<s.length(); i++){
-            window[s[i-k]-'a']--;
-            window[s[i]-'a']++;
-
-            if (phash == window){
-                ans.push_back(i-k+1);
-            }
+        int i=0, j=p.length();
+        while (j<s.length()){
+            window[s[j]-'a']+=1;
+            window[s[i]-'a']-=1;
+            i+=1;
+            if (window == ph) ans.push_back(i);
+            j+=1;
         }
 
         return ans;

109a6804e11659dbfb49d40214750388704100f1
Shashank Ranjan <shashankranjan07@gmail.com>
2024-05-30 22:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (59.87%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
+    bool permutation(vector<int> &v){
+        for (auto it:v){
+            if (it!=0) return false;
+        }
+
+        return true;
+    }
     bool checkInclusion(string s1, string s2) {
-        int k = s1.length();
-        if (s2.length()< k){
-            return false;
-        }
-        vector<int> window(26);
-        vector<int> hashmapp(26);
-
-        for (int i=0; i<k; i+=1){
-            window[s2[i] - 'a']+=1;
-            hashmapp[s1[i]-'a']+=1;
+        if (s1.length() > s2.length()) return false;
+        vector<int> mapp(26, 0);
+        for (int i=0; i<s1.length(); i+=1){
+            mapp[s1[i]-'a']+=1;
         }
 
-        if (window == hashmapp){
-            return true;
-        }
-
-        for (int i=k; i<s2.length(); i+=1){
-            window[s2[i] - 'a']+=1;
-            window[s2[i-k] - 'a']-=1;
-
-            if (window == hashmapp){
-                return true;
+        int i=0, j=0;
+        while (j<s2.length()){
+            mapp[s2[j]-'a']-=1;
+            if (j-i+1 == s1.length()){
+                if (permutation(mapp)){
+                    return true;
+                }
+                mapp[s2[i]-'a']+=1;
+                i+=1;
             }
+            j+=1;
         }
 
         return false;

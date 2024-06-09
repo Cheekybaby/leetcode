de3295bc375a5d0d6cb695e0f4dcac98aa789b28
Shashank Ranjan <shashankranjan07@gmail.com>
2024-06-09 09:23:12 +0530

    Time: 5 ms (21.01%) | Memory: 8.1 MB (44.29%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,14 +3,15 @@ public:
     double power(double x, long long n){
         if (n == 0) return 1.0;
 
-        if (n < 0) return 1.0/power(x, -n);
+        if (n<0) return 1.0/power(x, -n);
 
         if (n%2){
-            return x*power(x*x, (n-1)/2);
+            double result = power(x, n/2);
+            return result*result*x;
         } else {
-            return power(x*x, n/2);
+            double result = power(x, n/2);
+            return result*result;
         }
-
     }
     double myPow(double x, int n) {
         return power(x, (long long)n);

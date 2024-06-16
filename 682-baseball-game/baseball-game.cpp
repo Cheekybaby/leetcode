487faf30da94e03941764f71301e2cea471b22d8
Shashank Ranjan <shashankranjan07@gmail.com>
2024-06-16 14:25:57 +0530

    Time: 3 ms (68.15%) | Memory: 12 MB (10.11%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -2,38 +2,37 @@ class Solution {
 public:
     int calPoints(vector<string>& operations) {
         stack<int> st;
-        int size = operations.size(), i = 0;
-        while (size--){
-            string op = operations[i];
-            if (op == "C"){
-                st.pop();
-            } else if (op == "D"){
-                int num = st.top();
-                st.push(2*num);
-            } else if (op == "+"){
-                int arr[2];
-                for (int i=0; i<2; i+=1){
-                    arr[i] = st.top();
+        int i = 0;
+        while (i<operations.size()){
+            string c = operations[i];
+
+            if (c == "+"){
+                if (st.size()>1){
+                    int a, b;
+                    a = st.top();
                     st.pop();
+                    b = st.top();
+                    st.push(a);
+                    st.push(a+b);
                 }
-                int num = arr[0]+arr[1];
-                st.push(arr[1]);
-                st.push(arr[0]);
-                st.push(num);
+            } else if (c == "D"){
+                if (!st.empty()){
+                    st.push(2*(st.top()));
+                }
+            } else if (c == "C"){
+                if (!st.empty()) st.pop();
             } else {
-                int num = stoi(op);
-                st.push(num);
+                int x = stoi(c);
+                st.push(x);
             }
             i+=1;
         }
-
-
-        // Sum of all the scores
-        int sum = 0;
-        while (st.size()){
-            sum+=st.top();
+        int ans = 0;
+        while (!st.empty()){
+            ans += st.top();
             st.pop();
         }
-        return sum;
+
+        return ans;
     }
 };

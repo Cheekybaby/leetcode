b19cc14518655732725b49165d06ba25e330b34a
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-02 00:10:47 +0530

    Time: 2 ms (59.02%) | Memory: 7.3 MB (27.95%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+    void recursion(int n, bool &alice){
+        if (n == 0){
+            return ;
+        }
+
+        for (int i=1; i<n; i+=1){
+            if (n%i == 0){
+                alice=!alice;
+                recursion(n-i, alice);
+                break;
+            }
+        }
+    }
     bool divisorGame(int n) {
-        if (n%2 == 0) return true;
-        return false;
+        bool alice = true;
+        recursion(n, alice);
+        return !alice;
     }
 };

2dc12b8f1f52847de5c3f29f04f90c6e8552bc65
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-01 14:01:12 +0530

    Time: 49 ms (67.79%) | Memory: 48.8 MB (47.21%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n, memo = {}) {
+    if (n in memo) return memo[n];
+    if (n === 0 || n === 1){
+        return 1;
+    }
+
+    memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+
+    return memo[n];
+};

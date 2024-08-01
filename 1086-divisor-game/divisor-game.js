966985ed46dae5c146f3efdd614b31f3271ec81a
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-02 00:13:51 +0530

    Time: 58 ms (25.94%) | Memory: 48.6 MB (75.67%) - LeetSync

--- 1086-divisor-game/divisor-game.js
+++ 1086-divisor-game/divisor-game.js
@@ -2,7 +2,20 @@
  * @param {number} n
  * @return {boolean}
  */
+const recur = (n, alice) => {
+    if (n === 0){
+        return ;
+    }
+    for (let i=1; i<n; i+=1){
+        if (n%i === 0){
+            alice[0] = !alice[0];
+            recur(n-i, alice);
+            break;
+        }
+    }
+}
 var divisorGame = function(n) {
-    if (n%2 === 0) return true;
-    return false;
+    let alice = [true];
+    recur(n, alice);
+    return !alice[0];
 };

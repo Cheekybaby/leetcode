213041252129be0abe829e3a25ad18a35b491d66
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-05 15:56:51 +0530

    Time: 353 ms (5.04%) | Memory: 66.7 MB (6.68%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int> &coins, int amount, unordered_map<int,int> &memo){
+        if (amount < 0) return INT_MAX;
+        if (amount == 0) return 0;
+        if (memo.find(amount) != memo.end()) return memo[amount];
+
+        int minCoins = INT_MAX;
+
+        for (int i=0; i<coins.size(); i+=1){
+            int res = solve(coins, amount - coins[i], memo);
+
+            if (res != INT_MAX){
+                minCoins = min(minCoins, 1 + res);
+            }
+        }
+        memo[amount] = minCoins;
+        return memo[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        if (amount == 0) return 0;
+        unordered_map<int,int> memo;
+        int ans = solve(coins, amount, memo);
+        if (ans == INT_MAX){
+            return -1;
+        }
+        return ans;
+    }
+};

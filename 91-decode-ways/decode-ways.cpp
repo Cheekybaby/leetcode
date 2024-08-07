3cdd1175bbbec1c3a5c94ea6b93bf088244a080e
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-07 13:00:22 +0530

    Time: 6 ms (10.52%) | Memory: 10.7 MB (17.35%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int solve(string s, int idx, vector<int> &memo){
-        if (idx == s.length()) {
-            return 1;
-        }
-        if (s[idx] == '0'){
-            return 0;
-        }
-        if (memo[idx] != -1){
-            return memo[idx];
+    int solve(string s, int index, vector<int> &memo){
+        if (index > s.length()) return 0;
+        if (index == s.length()) return 1;
+        if (s[index] == '0') return 0;
+        if (memo[index] != -1) return memo[index];
+
+        int ways = solve(s, index + 1, memo);
+        if (index < s.length() && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6'))){
+            ways += solve(s, index + 2, memo);
         }
 
-        int ways = solve(s, idx+1, memo);
-        if (idx+1 < s.length() && ((s[idx] == '1') || (s[idx] == '2' &&  s[idx+1] <= '6'))){
-            ways += solve(s, idx+2, memo);
-        }
-        memo[idx] = ways;
-        return memo[idx];
+        memo[index] = ways;
+        return ways;
     }
     int numDecodings(string s) {
         vector<int> memo(s.length(), -1);

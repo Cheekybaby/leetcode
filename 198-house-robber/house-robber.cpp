37797097e33b0a7b0905b4734ab0df230acc5451
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-07 13:09:22 +0530

    Time: 4 ms (31.55%) | Memory: 9.6 MB (23.34%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int n, vector<int> &memo){
-        if (n < 0) return 0;
-        if (n == 0) return nums[0];
-        if (memo[n] != -1) return memo[n];
+    int solve(vector<int> &nums, int index, vector<int> &memo){
+        if (index < 0) return 0;
+        if (index == 0) return nums[0];
+        if (memo[index] != -1) return memo[index];
 
-        int picked = nums[n] + solve(nums, n - 2, memo);
-        int notPicked = solve(nums, n - 1, memo);
+        int loots = nums[index] + solve(nums, index - 2, memo);
+        int not_loots = solve(nums, index - 1, memo);
 
-        memo[n] = max(picked, notPicked);
-        return memo[n];
+        memo[index] = max(loots, not_loots);
+        return memo[index]; 
     }
     int rob(vector<int>& nums) {
         vector<int> memo(nums.size(), -1);

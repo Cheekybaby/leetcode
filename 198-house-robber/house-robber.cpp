ac62ed604dd62d62c654347fc98c9ff57c9ac35b
Shashank Ranjan <shashankranjan07@gmail.com>
2024-08-09 12:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (7.68%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int solve(vector<int> &nums, int index, vector<int> &memo){
-        if (index < 0) return 0;
         if (index == 0) return nums[0];
+        if (index < 0) return 0;
         if (memo[index] != -1) return memo[index];
+        int robs = nums[index] + solve(nums, index - 2, memo);
+        int not_robs = solve(nums, index - 1, memo);
 
-        int loots = nums[index] + solve(nums, index - 2, memo);
-        int not_loots = solve(nums, index - 1, memo);
-
-        memo[index] = max(loots, not_loots);
-        return memo[index]; 
+        memo[index] = max(robs, not_robs);
+        return memo[index];
     }
     int rob(vector<int>& nums) {
         vector<int> memo(nums.size(), -1);

0c214d5958f3288e5df6921efa683c2a0188b24a
Shashank Ranjan <shashankranjan07@gmail.com>
2024-09-17 11:20:40 +0530

    Time: 11 ms (41.66%) | Memory: 18.7 MB (54.56%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mpp;
+        int cand1 = -1, vote1 = 0;
+        int cand2 = -1, vote2 = 0;
+
         for (int i=0; i<nums.size(); i+=1){
-            mpp[nums[i]]+=1;
+            if (nums[i] == cand1){
+                vote1 += 1;
+            } else if (nums[i] == cand2){
+                vote2 += 1;
+            } else if (vote1 == 0){
+                cand1 = nums[i];
+                vote1 += 1;
+            } else if (vote2 == 0){
+                cand2 = nums[i];
+                vote2 += 1;
+            } else {
+                vote1 -= 1;
+                vote2 -= 1;
+            }
+        }
+
+        vote1 = 0, vote2 = 0;
+
+        for (auto it:nums){
+            if (it == cand1){
+                vote1 += 1;
+            } else if (it == cand2){
+                vote2 += 1;
+            }
         }
         vector<int> ans;
-        for (auto it:mpp){
-            if (it.second > nums.size()/3){
-                ans.push_back(it.first);
-            }
+        if (vote1 > nums.size()/3){
+            ans.push_back(cand1);
+        }
+        if (vote2 > nums.size()/3){
+            ans.push_back(cand2);
         }
 
         return ans;

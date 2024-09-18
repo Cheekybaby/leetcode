e06d17f05d1b7d365b5e50fee9f37f99c22cdeb4
Shashank Ranjan <shashankranjan07@gmail.com>
2024-09-18 16:38:53 +0530

    Time: 194 ms (14.13%) | Memory: 52.3 MB (17.37%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,31 +1,32 @@
-using ll = long long;
+#define ll long long
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
         set<vector<int>> st;
-        for(int i=0; i<n-3; i+=1){
-            for (int j=i+1; j<n-2; j+=1){
-                ll newtarget = ll(target) - ll(nums[i]) - ll(nums[j]);
-                int k = j+1, l = n-1;
-                while (k<l){
-                    if (nums[k]+nums[l]<newtarget){
-                        k+=1;
-                    } else if (nums[k]+nums[l]>newtarget){
-                        l-=1;
+        if (nums.size() < 4) return {};
+        for (int i=0; i<nums.size()-3; i+=1){
+            for (int j=i+1; j<nums.size()-2; j+=1){
+                ll toFind = (ll)target - (ll)(nums[i] + nums[j]);
+                int l = j+1, r = nums.size()-1;
+                while (l < r){
+                    int sum = nums[l] + nums[r];
+
+                    if (sum == toFind){
+                        st.insert({nums[i], nums[j], nums[l], nums[r]});
+
+                        l+=1;
+                        r-=1;
+                    } else if (sum > toFind){
+                        r-=1;
                     } else {
-                        st.insert({nums[i], nums[j], nums[k], nums[l]});
-                        k+=1;
-                        l-=1;
+                        l+=1;
                     }
                 }
             }
         }
-        for (auto it:st){
-            ans.push_back(it);
-        }
+
+        vector<vector<int>> ans(st.begin(), st.end());
 
         return ans;
     }

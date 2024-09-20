830be4c15426d7d0a02e693f3a1b8e1ec4f33912
Shashank Ranjan <shashankranjan07@gmail.com>
2024-09-20 17:34:41 +0530

    Time: 3 ms (86.20%) | Memory: 16.5 MB (80.17%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -10,42 +10,44 @@
  */
 class Solution {
 public:
-    int lengthofLL(ListNode *head){
+    int lenOfLL(ListNode *head){
         int len = 0;
-        ListNode *curr = head;
-        while (curr){
-            len+=1;
-            curr = curr->next;
+        ListNode* temp = head;
+        while (temp){
+            len += 1;
+            temp = temp->next;
         }
 
         return len;
     }
     ListNode* rotateRight(ListNode* head, int k) {
         if (head == nullptr || head->next == nullptr) return head;
-        int len = lengthofLL(head);
-        k%=len;
-        if (k == 0) return head;
+        int len = lenOfLL(head);
 
-        ListNode *slow = head;
-        ListNode *fast = head;
-        int step = k;
-        while (step){
+        k %= len;
+        if (k == 0) return head;
+        ListNode *slow = head, *fast = head;
+
+        while (k--){
             fast = fast->next;
-            step-=1;
         }
-        while (fast && fast->next){
+
+        while (fast->next != nullptr){
             slow = slow->next;
             fast = fast->next;
         }
 
-        ListNode *newhead = slow->next;
+        ListNode *temp = slow->next;
         slow->next = nullptr;
-        ListNode *temp = newhead;
-        while (temp->next){
-            temp = temp->next;
-        }
-        temp->next = head;
+        fast = temp;
 
-        return newhead;
+        while (fast->next != nullptr){
+            fast = fast->next;
+        }
+
+        fast->next = head;
+
+
+        return temp;
     }
 };

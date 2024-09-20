dfef2cab250f00b5b064562e1897699b06de2bc3
Shashank Ranjan <shashankranjan07@gmail.com>
2024-09-20 18:10:26 +0530

    Time: 43 ms (99.24%) | Memory: 27.5 MB (74.70%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,45 +1,30 @@
-/*
-- Sort the array
-- Iterate through the array taking one element once and finding the other two in the rest of the array to the right.
-- What are cases we need to avoid?
-    - We need to make sure that we don't use up the same element more than once in a possible triplet.
-    - For no possible duplicate triplets, we skip all the same elements we get while traversal.
-    - One more way is to create a set of vector so that we don't get the redundancy. Otherwise, we can just skip the elements.
-
-- Whenever we find a possible triplet we sort it in ascending order and then push it into the answer.
-- 
-*/
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-
+        sort(nums.begin(), nums.end());
         for (int i=0; i<nums.size(); i+=1){
-            if (i != 0 && nums[i] == nums[i-1]) continue; // Skipping the same element
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            int l = i+1, r = nums.size()-1;
 
-            int j = i+1 , k = nums.size()-1;
-            while (j<k){
-                int sum = nums[i] + nums[j] + nums[k];
+            while (l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+
                 if (sum == 0){
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    sort(temp.begin(), temp.end());
-                    ans.push_back(temp);
+                    ans.push_back({nums[i], nums[l], nums[r]});
 
-                    j+=1;
-                    k-=1;
+                    l+=1;
+                    r-=1;
 
-                    while (j<k && nums[j] == nums[j-1]) j+=1;
-                    while (j<k && nums[k] == nums[k+1]) k-=1;
+                    while(l<r && nums[l] == nums[l-1]) l+=1;
+                    while(l<r && nums[r] == nums[r+1]) r-=1;
                 } else if (sum > 0){
-                    k-=1;
+                    r-=1;
                 } else {
-                    j+=1;
+                    l+=1;
                 }
             }
         }
-
         return ans;
     }
 };

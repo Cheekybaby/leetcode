750e9f8586e74dd1e51f6d1b16647a8a461b3cf1
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-05 12:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (94.59%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -15,17 +15,16 @@ public:
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        /*
+        1. Recursion and Memoization
         int ans = 100001;
+        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        for(int i=0; i<n; i+=1){
+            ans = min(ans, solve(matrix, 0, i, dp));
+        }
+        return ans;
         
-        // Recursion and Memoization
-        
-        // vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        // for(int i=0; i<n; i+=1){
-        //     ans = min(ans, solve(matrix, 0, i, dp));
-        // }
-        // return ans;
-
-        // Tabulation
+        2. Tabulation
         vector<vector<int>> dp(n, vector<int> (n, 0));
         for(int i=0; i<n; i+=1){
             dp[0][i] = matrix[0][i];
@@ -41,6 +40,25 @@ public:
             }
         }
 
-        return *min_element(dp[n-1].begin(), dp[n-1].end());
+        return *min_element(dp[n-1].begin(), dp[n-1].end()); */
+
+        // 3. Tabulation + Space Optimization
+        vector<int> prev(n, 0);
+        for(int i=0; i<n; i+=1){
+            prev[i] = matrix[0][i];
+        }
+        vector<int> dp(n, 0);
+        for(int i=1; i<n; i+=1){
+            for(int j=0; j<n; j+=1){
+                int ldiag = (j > 0) ? prev[j-1] : 100001;
+                int up = prev[j];
+                int rdiag = (j < n-1) ? prev[j+1] : 100001;
+
+                dp[j] = matrix[i][j] + min(ldiag, min(up, rdiag));
+            }
+            prev = dp;
+        }
+
+        return *min_element(prev.begin(), prev.end());
     }
 };

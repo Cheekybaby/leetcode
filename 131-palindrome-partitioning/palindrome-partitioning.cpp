878fbbcf8f5e949ed75353d0f3effb566a91141b
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-08 18:08:25 +0530

    Time: 71 ms (44.68%) | Memory: 79.4 MB (34.22%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<string> temp;
-        vector<vector<string>> ans;
-        int index = 0;
-        solve(s, ans, temp, index);
-        return ans;
-    }
+    // Global
+    vector<vector<string>> ans;
+    vector<string> temp;
 
-private:
-    void solve(string &s, vector<vector<string>> &ans, vector<string> &temp, int index){
-        if (index >= s.size()){
+    void solve(string &s, int index){
+        if (index >= s.length()) {
             ans.push_back(temp);
             return ;
         }
 
-        for (int i=index; i<s.size(); i+=1){
+        for (int i=index; i<s.length(); i+=1){
             if (isPalindrome(s, index, i)){
                 temp.push_back(s.substr(index, i-index+1));
-                solve(s, ans, temp, i+1);
+                solve(s, i+1);
                 temp.pop_back();
             }
         }
     }
+    vector<vector<string>> partition(string s) {
+        solve(s, 0);
 
-    bool isPalindrome(string &s, int start, int end){
-        while (start <= end){
-            if (s[start] != s[end]){
+        return ans;
+    }
+
+    bool isPalindrome(string &s, int st, int end){
+        while (st <= end){
+            if (s[st] != s[end]){
                 return false;
             }
-            start+=1;
+            st+=1;
             end-=1;
         }
 

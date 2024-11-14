58e5bbe32282736d1a791a76fac45a879b8a2ca4
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-14 15:57:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (92.96%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1) return s;
-        vector<int> rows(s.length(), 0);
-        int k = 1;
-        bool flip = false;
-        for(int i=0; i<s.length(); i+=1){
-            // Check if the series should be incrementing or decrementing
-            if (k == numRows){
-                flip = true;
-            } else if (k == 1){
-                flip = false;
-            }
-
-            // Update rows
-            rows[i] = k;
-            
-            // Update the value of k
-            if (!flip){
-                k+=1;
-            } else {
-                k-=1;
-            }
-
-        }
-        for(auto &it:rows){
-            cout << it << " ";
-        }
+        // Edge cases
+        if (numRows == 1 || numRows >= s.length()) return s;
+        // Output String
         string output;
-        int c=1;
-        while (numRows--){
-            for(int i=0; i<rows.size(); i+=1){
-                if (rows[i] == c){
-                    output += s[i];
+        
+        int step = 2*(numRows-1);
+
+        for(int i=0; i<numRows; i+=1){
+            for(int j=i; j<s.length(); j+=step){
+                output += s[j];
+
+                // Diagonal
+                int diag = j + step - 2*i;
+                if (i > 0 && i < numRows-1 && diag < s.length()){
+                    output += s[diag];
                 }
             }
-            c+=1;
         }
 
         return output;

37b0a83e98bd4858c42104d74ab84906ced58e43
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-14 10:44:21 +0530

    Time: 222 ms (25.68%) | Memory: 26.7 MB (38.07%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,35 +1,46 @@
+/*
+- To make it n*n we need to make the palindrome checking a constant time operation.
+- We can build a matrix before hand that can tell us if a substring is palindrome or not in constant time.
+- The matrix will be of boolean values and its rows and matrix will keep the record of the particular string being palindrome or not.
+
+*/
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int ans = 0;
-        string longest;
-        for (int i=0; i<s.length(); i+=1){
-            string temp = "";
-            for (int j=i; j<s.length(); j+=1){
-                temp.push_back(s[j]);
-                if(isPalindrome(temp)){
-                    if (temp.length() > ans){
-                        ans = temp.length();
-                        longest = temp;
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool> (n, false));
+
+        // for maxlength = 1
+        int maxlen = 1;
+        int start = 0;
+        for(int i=0; i<n; i+=1){
+            dp[i][i] = true;
+        }
+
+        // for maxlength = 2
+        for(int i=0; i<n-1; i+=1){
+            if (s[i] == s[i+1]){
+                start = i;
+                dp[i][i+1] = true;
+                maxlen = 2;
+            }
+        }
+
+        // for maxlength >= 3
+        for (int k=3; k<=n; k+=1){
+            for (int i=0; i<n-k+1; i+=1){
+                int j = i+k-1;
+
+                if (dp[i+1][j-1] && s[i] == s[j]){
+                    dp[i][j] = true;
+                    if (k >= maxlen){
+                        maxlen = k;
+                        start = i;
                     }
                 }
             }
         }
-        return longest;
-    }
-private:
-    bool isPalindrome(string &s){
-        int i = 0;
-        int j = s.length()-1;
 
-        while (i < j){
-            if (s[i] != s[j]){
-                return false;
-            }
-            i+=1;
-            j-=1;
-        }
-
-        return true;
+        return s.substr(start, maxlen);
     }
 };

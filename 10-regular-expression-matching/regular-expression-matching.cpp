4ac28753db5d6b40160b352078aa8da630431f1a
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-14 19:03:26 +0530

    Time: 2074 ms (5.05%) | Memory: 10.5 MB (19.60%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -10,13 +10,14 @@ public:
         bool firstMatch = (i < s.length() && (s[i] == p[j] || p[j] == '.'));
 
         if (j + 1 < p.length() && p[j+1] == '*'){
-            return memo[i][j] = (solve(s, p, i, j+2, memo) || (firstMatch && solve(s, p, i+1, j, memo)));
+            memo[i][j] = (solve(s, p, i, j+2, memo) || (firstMatch && solve(s, p, i+1, j, memo)));
         } else {
-            return memo[i][j] = firstMatch && solve(s, p, i+1, j+1, memo);
+            memo[i][j] = firstMatch && solve(s, p, i+1, j+1, memo);
         }
+        return memo[i][j];
     }
     bool isMatch(string s, string p) {
-        if (s == p) return true;
+        // if (s == p) return true;
         vector<vector<bool>> dp(s.length()+1, vector<bool> (p.length()+1, false));
         return solve(s, p, 0, 0, dp);
     }

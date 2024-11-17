0b0cc13b5b66d9a263a533bc5c9f2a86773d2b45
Shashank Ranjan <shashankranjan07@gmail.com>
2024-11-17 11:46:12 +0530

    Time: 45 ms (82.96%) | Memory: 27.5 MB (49.52%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -9,22 +9,22 @@ public:
             int l = i+1, r = nums.size()-1;
             while (l < r){
                 int sum = nums[i] + nums[l] + nums[r];
-                if (sum == 0) {
+
+                if (sum == 0){
                     ans.push_back({nums[i], nums[l], nums[r]});
 
                     l+=1;
                     r-=1;
 
-                    while (l<r && nums[l] == nums[l-1]) l+=1;
-                    while (l<r && nums[r] == nums[r+1]) r-=1;
-                } else if (sum > 0) {
+                    while(l<r && nums[l] == nums[l-1]) l+=1;
+                    while(l<r && nums[r] == nums[r+1]) r-=1;
+                } else if (sum > 0){
                     r-=1;
                 } else {
                     l+=1;
                 }
             }
         }
-
         return ans;
     }
 };

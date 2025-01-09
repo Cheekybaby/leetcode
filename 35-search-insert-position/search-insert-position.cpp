a874efe451e299d8528ad5da3ddbef25949dc569
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-09 13:51:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (28.89%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int left = 0, right = nums.size()-1;
-        while (left <= right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) right = mid - 1;
-            else left = mid + 1;
-        }
-
-        return left;
+        int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return l;
     }
 };

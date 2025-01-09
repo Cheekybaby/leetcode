2e16aad92e4c64e6ba481afbf17037a844f1db7d
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-09 12:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (74.56%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,10 @@
 class Solution {
 public:
-    int lowerbound(vector<int> &nums, int x){
-        int left = 0, right = nums.size()-1;
-        int ans = -1;
-        while (left<=right){
-            int mid = left + (right - left) /2;
-            if (nums[mid] == x){
-                ans = mid;
-                right = mid - 1;
-            } else if (nums[mid] > x) right = mid - 1;
-            else left = mid + 1; 
-        }
-
-        return ans;
-    }
-    int upperbound(vector<int> &nums, int x){
-        int left = 0, right = nums.size()-1;
-        int ans = -1;
-        while (left <= right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == x){
-                ans = mid;
-                left = mid + 1;
-            } else if (nums[mid] > x) right = mid - 1;
-            else left = mid + 1;
-        }
-
-        return ans;
-    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = lowerbound(nums, target);
-        int r = upperbound(nums, target);
-        return {l, r};
+        if (!binary_search(nums.begin(), nums.end(), target)) return {-1,-1}; // Element isn't present
+        int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin(); // First Occurance
+        int r = upper_bound(nums.begin(), nums.end(), target) - nums.begin(); // Last Occurance
+        return {l, r-1};
     }
 };

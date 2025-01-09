2695ccbff660fb80a9572360e99e560eb518f380
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-09 13:50:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (39.86%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,10 +1,46 @@
 class Solution {
 public:
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        if (!binary_search(nums.begin(), nums.end(), target)) return {-1,-1}; // Element isn't present
-        int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin(); // First Occurance
-        int r = upper_bound(nums.begin(), nums.end(), target) - nums.begin(); // Last Occurance
-        return {l, r-1};
+        int l = lower_bound(nums, target);
+        int r = upper_bound(nums, target);
+        return {l, r};
+    }
+
+private:
+    int lower_bound(vector<int> &nums, int target){
+        int l = 0, r = nums.size()-1;
+        while(l <= r){
+            int mid = midpoint(l, r);
+            if (nums[mid] == target){
+                if (mid == 0) return mid;
+                else {
+                    if (nums[mid-1] != target) return mid;
+                    else r = mid - 1;
+                }
+            } else if (nums[mid] > target){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+    int upper_bound(vector<int> &nums, int target){
+        int l = 0, r = nums.size()-1;
+        while(l <= r){
+            int mid = midpoint(l, r);
+            if (nums[mid] == target){
+                if (mid == nums.size()-1) return mid;
+                else {
+                    if (nums[mid+1] != target) return mid;
+                    else l = mid + 1;
+                }
+            } else if (nums[mid] > target){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
     }
 };

3865ce62a9000a3e6082a7fddac0433b8e8c6781
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-09 14:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (26.51%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -8,12 +8,11 @@ public:
         while(l <= r){
             long long mid = midpoint(l, r);
             if (isBadVersion(mid) == true){
-                if (isBadVersion(mid-1) == false) return mid;
-                else r = mid-1;
+                r = mid-1;
             } else {
                 l = mid + 1;
             }
         }
-        return -1;
+        return l;
     }
 };

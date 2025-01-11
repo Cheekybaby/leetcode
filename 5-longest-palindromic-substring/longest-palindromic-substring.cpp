c7eb606346f2c618e5c5e3ff71145cf4c7214308
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-11 22:09:03 +0530

    Time: 191 ms (26.99%) | Memory: 292 MB (9.50%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,46 +1,33 @@
-/*
-- To make it n*n we need to make the palindrome checking a constant time operation.
-- We can build a matrix before hand that can tell us if a substring is palindrome or not in constant time.
-- The matrix will be of boolean values and its rows and matrix will keep the record of the particular string being palindrome or not.
-
-*/
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        vector<vector<bool>> dp(n, vector<bool> (n, false));
-
-        // for maxlength = 1
-        int maxlen = 1;
-        int start = 0;
-        for(int i=0; i<n; i+=1){
-            dp[i][i] = true;
+        vector<vector<int>> vc(s.length(), vector<int>(s.length(), 0));
+        int len = 1, start = 0;
+        // Length 1 substring
+        for (int i = 0; i < s.length(); i += 1) {
+            vc[i][i] = 1;
         }
 
-        // for maxlength = 2
-        for(int i=0; i<n-1; i+=1){
-            if (s[i] == s[i+1]){
+        // Length 2 substring
+        for (int i = 0; i < s.length() - 1; i += 1) {
+            if (s[i] == s[i + 1]) {
                 start = i;
-                dp[i][i+1] = true;
-                maxlen = 2;
+                len = 2;
+                vc[i][i + 1] = 1;
             }
         }
 
-        // for maxlength >= 3
-        for (int k=3; k<=n; k+=1){
-            for (int i=0; i<n-k+1; i+=1){
-                int j = i+k-1;
-
-                if (dp[i+1][j-1] && s[i] == s[j]){
-                    dp[i][j] = true;
-                    if (k >= maxlen){
-                        maxlen = k;
-                        start = i;
-                    }
+        // Length > 2 substring
+        for (int k = 3; k < s.length() + 1; k += 1) {
+            for (int i = 0; i < s.length() - k + 1; i += 1) {
+                int j = i + k - 1;
+                if (s[i] == s[j] && vc[i + 1][j - 1]) {
+                    vc[i][j] = 1;
+                    len = max(len, k);
+                    start = i;
                 }
             }
         }
-
-        return s.substr(start, maxlen);
+        return s.substr(start, len);
     }
 };

069a0b1f9c52a57b8f34ecaff09b07e98b0c2f3f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-13 14:30:37 +0530

    Time: 12 ms (73.39%) | Memory: 32.1 MB (58.06%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char, int> m;
+        vector<int> m(26);
         for (int i = 0; i < s.length(); i += 1) {
-            m[s[i]]++;
+            m[s[i]-'a']++;
         }
         int len = 0;
         for (auto& it : m) {
-            int x = it.second;
+            int x = it;
             if (x > 2) {
                 if (x % 2 == 1) {
                     len += 1;

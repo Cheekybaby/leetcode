047113107eeb337cdb0453b228295a6b98d132a3
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-17 12:01:28 +0530

    Time: 8 ms (25.70%) | Memory: 263.9 MB (48.13%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        vector<int> a = {0};
-        for(int i=0; i<derived.size(); i+=1){
-            a.push_back(a[i] ^ derived[i]);
+        int xo = 0;
+        for(int i=0; i<derived.size(); i++){
+            xo ^= derived[i];
         }
 
-        bool checkForZero = (a[0] == a[a.size()-1]);
-
-        vector<int> b = {1};
-        for(int i=0; i<derived.size(); i+=1){
-            b.push_back(b[i] ^ derived[i]);
-        }
-
-        bool checkForOne = (b[0] == b[b.size()-1]);
-
-        return checkForZero | checkForOne;
+        return !xo;
     }
 };

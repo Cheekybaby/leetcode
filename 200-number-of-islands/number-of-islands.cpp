61206552dcb4328afc54b507d3de6340bb0305d7
Shashank Ranjan <shashankranjan07@gmail.com>
2025-02-04 10:33:25 +0530

    Time: 128 ms (5.11%) | Memory: 33.9 MB (7.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        int numberOfIslands = 0;
+        int r = grid.size();
+        int c = grid[0].size();
+        unordered_set<string> visited;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == '1' &&
+                    visited.find((to_string(i) + "," + to_string(j))) ==
+                        visited.end()) {
+                    numberOfIslands++;
+                    bfs(grid, visited, i, j);
                 }
             }
         }
-        
-        return numIslands;
+        return numberOfIslands;
     }
-    
+
 private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
-            return;
+    vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    
+    void bfs(vector<vector<char>>& grid, unordered_set<string>& visited, int r,
+             int c) {
+        string coor = to_string(r) + "," + to_string(c);
+        visited.insert(coor);
+        queue<pair<int, int>> q;
+        q.push({r, c});
+
+        while (!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+            for (auto [l, r] : dir) {
+                int rr = row + l;
+                int cc = col + r;
+
+                if (rr >= 0 && rr < grid.size() && cc >= 0 &&
+                    cc < grid[0].size() && grid[rr][cc] == '1' &&
+                    visited.find(to_string(rr) + "," + to_string(cc)) ==
+                        visited.end()) {
+                    q.push({rr, cc});
+                    visited.insert(to_string(rr) + "," + to_string(cc));
+                }
+            }
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
     }
 };

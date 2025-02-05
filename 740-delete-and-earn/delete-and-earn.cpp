5829317cc78537eba9a43a3ab1dbcc60e11805c0
Shashank Ranjan <shashankranjan07@gmail.com>
2025-02-05 12:57:05 +0530

    Time: 19 ms (11.52%) | Memory: 25.8 MB (5.20%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int index, vector<int> &memo){
-        if (index >= nums.size()) return 0;
-        if (memo[index] != -1) return memo[index];
-
-        int i = index;
-        int sum = 0;
-        while(i < nums.size() && nums[i] == nums[index]){
-            sum += nums[i];
-            i+=1;
-        }
-        while (i < nums.size() && (nums[i] == nums[index]+1)){
-            i+=1;
-        }
-        int take = sum + solve(nums, i, memo);
-        int not_take = solve(nums, index+1, memo);
-
-        return memo[index] = max(take, not_take);
-    }
     int deleteAndEarn(vector<int>& nums) {
+        int ans = 0;
         sort(nums.begin(), nums.end());
-        vector<int> dp(nums.size(), -1);
+        vector<int> acc(20000);
+        for(int i=0; i<nums.size();){
+            int sum = nums[i];
+            int index = nums[i];
+            i++;
+            while(i < nums.size() && nums[i] == nums[i-1]){
+                sum += nums[i];
+                i++;
+            }
 
-        return solve(nums,0, dp);
+            acc[index] = sum;
+        }
+
+        ans = maximumSum(acc);
+
+        return ans;
+    }
+
+private:
+    int maximumSum(vector<int> &acc){
+        vector<int> dp(acc.size()+1, -1);
+        return solve(acc, 0, dp);
+    }
+    int solve(vector<int> &acc, int index, vector<int> &dp){
+        if (index >= acc.size()) return 0;
+
+        if (dp[index] != -1) return dp[index];
+
+        int take = acc[index] + solve(acc, index+2, dp);
+        int not_take = solve(acc, index+1, dp);
+
+        return dp[index] = max(take, not_take);
     }
 };

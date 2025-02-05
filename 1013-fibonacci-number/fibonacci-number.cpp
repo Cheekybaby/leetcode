7820816a067adfa0830d3358e7d4fa9ab2c7bc92
Shashank Ranjan <shashankranjan07@gmail.com>
2025-02-05 12:18:50 +0530

    Time: 14 ms (23.04%) | Memory: 7.8 MB (34.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    int arr[31];
-    bool check[31];
-
-
     int fib(int n) {
-        if (check[n]) return arr[n];
         if (n == 0) return 0;
-        if (n <= 2) return 1;
+        if (n == 1) return 1;
 
-        return arr[n] = fib(n-1) + fib(n-2);    
+        return fib(n-1) + fib(n-2);
     }
 };

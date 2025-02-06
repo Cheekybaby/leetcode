ceb9649c918ed21a47d277d1ea9721734090bed4
Shashank Ranjan <shashankranjan07@gmail.com>
2025-02-06 17:36:12 +0530

    Time: 8 ms (45.49%) | Memory: 13.1 MB (60.10%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int lev(string &word1, string &word2, int i, int j, vector<vector<int>> &dp){
-        if (i == word1.length()) return word2.length() - j;
-        if (j == word2.length()) return word1.length() - i;
-        if (dp[i][j] != -1) return dp[i][j];
+    int solve(string& word1, string& word2, int i, int j,
+              vector<vector<int>>& dp) {
+        if (i == word1.length())
+            return word2.length() - j;
+        if (j == word2.length())
+            return word1.length() - i;
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-
-        if (word1[i] == word2[j]){
-            return lev(word1, word2, i+1, j+1, dp);
+        if (word1[i] == word2[j]) {
+            dp[i][j] = solve(word1, word2, i + 1, j + 1, dp);
+        } else {
+            dp[i][j] = 1 + min(solve(word1, word2, i + 1, j, dp),
+                               min(solve(word1, word2, i, j + 1, dp),
+                                   solve(word1, word2, i + 1, j + 1, dp)));
         }
-
-        return dp[i][j] = 1 + min(lev(word1, word2, i+1, j, dp), min(lev(word1, word2, i, j+1, dp), lev(word1, word2, i+1, j+1, dp)));
+        return dp[i][j];
     }
     int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return lev(word1, word2, 0, 0, dp);
+        int n = word1.length(), m = word2.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solve(word1, word2, 0, 0, dp);
     }
 };

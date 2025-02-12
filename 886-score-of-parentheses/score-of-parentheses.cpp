8bcd315eeb65b3592f0d356aa03fa48202cd5258
Shashank Ranjan <shashankranjan07@gmail.com>
2025-02-12 10:02:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (95.70%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.cpp
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        vector<string> store;
-        string temp;
-        int open = 0, close = 0;
-        for(int i=0; i<s.length(); i++){
-            temp += s[i];
-            if(s[i] == '('){
-                open++;
-            } else {
-                close++;
-            }
-
-            if (open - close == 0){
-                store.push_back(temp);
-                temp = "";
-            }
-        }
-        long long ans = 0;
-        for(auto &item:store){
-            string x = item;
-            ans += solve(x);
-        }
-        return ans;
-    }
-
-private:
-    int solve(string &s){
         stack<int> st;
         int score = 0;
         for(int i=0; i<s.length(); i++){

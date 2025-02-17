46cc33cc72ac1bf1906bfed284b4a4397d2f90b4
Shashank Ranjan <shashankranjan07@gmail.com>
2025-02-17 10:35:41 +0530

    Time: 1 ms (75.75%) | Memory: 11.3 MB (95.89%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, set<vector<int>> &sol, unordered_map<int,int> &m, vector<int> &temp, int index){
-        if (index == nums.size()) {
-            sol.insert(temp);
-            return ;
-        }
-
-        for(auto &it:nums){
-            if (m[it] > 0){
-                temp.push_back(it);
-                m[it]--;
-                solve(nums, sol, m, temp, index+1);
-                temp.pop_back();
-                m[it]++;
-            }
-        }
-    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> sol; // To store all the unique permutations
-        unordered_map<int,int> m; // To keep count of each occurance
-        for(auto &n:nums){
-            m[n]++;
-        }
-        vector<int> temp;
-
-        solve(nums, sol, m, temp, 0);
-
         vector<vector<int>> ans;
-        for(auto &it:sol){
-            ans.push_back(it);
-        }
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size(), false);
+
+        solve(nums, ans, temp, used, 0);
 
         return ans;
+    }
+
+    void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> &temp, vector<bool> &used, int index){
+        if (index == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if (used[i] || (i > 0 && nums[i] == nums[i-1] && !used[i-1])) continue;
+
+            temp.push_back(nums[i]);
+            used[i] = true;
+            solve(nums, ans, temp, used, index+1);
+            used[i] = false;
+            temp.pop_back();
+        }
     }
 };

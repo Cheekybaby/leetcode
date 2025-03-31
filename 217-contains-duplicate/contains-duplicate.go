422157aa5227e749c053cd72991b907f7159ae8f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-03-31 11:46:39 +0530

    Time: 15 ms (55.13%) | Memory: 11.7 MB (39.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -1,10 +1,11 @@
 func containsDuplicate(nums []int) bool {
-    sort.Ints(nums)
+    seen := make(map[int]bool)
 
-    for i:=0; i<len(nums)-1; i++ {
-        if nums[i] == nums[i+1] {
-            return true
+    for i:=0; i<len(nums); i++ {
+        if seen[nums[i]] {
+            return true;
         }
+        seen[nums[i]] = true;
     }
 
     return false;

663e6827034f28244983f1954027a65437730e8f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-03-31 13:06:33 +0530

    Time: 2 ms (57.40%) | Memory: 4.6 MB (12.94%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.go
+++ 1894-merge-strings-alternately/merge-strings-alternately.go
@@ -1,32 +1,24 @@
 func mergeAlternately(word1 string, word2 string) string {
-    rune1, rune2 := []rune(word1), []rune(word2)
-    result := make([]rune, len(word1) + len(word2))
-
-    n := 0
-    if len(word1) < len(word2) {
-        n = len(word1)
-    } else {
-        n = len(word2)
-    }
-
     i, j := 0, 0
-    for i = 0; i<n; i++ {
-        result[j] = rune1[i]
-        result[j+1] = rune2[i]
-        j+=2
+    var result string
+    for i < len(word1) && j < len(word2) {
+        if i <= j {
+            result += string(word1[i])
+            i+=1
+        } else {
+            result += string(word2[j])
+            j+=1
+        }
     }
 
     for i < len(word1) {
-        result[j] = rune1[i]
+        result += string(word1[i])
         i+=1
+    }
+    for j < len(word2) {
+        result += string(word2[j])
         j+=1
     }
 
-    for i < len(word2) {
-        result[j] = rune2[i]
-        i+=1
-        j+=1
-    }
-
-    return string(result)
+    return result
 }

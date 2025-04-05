159ec964d90e3d4e3c3dd954ad08e0fd139e5b86
Shashank Ranjan <shashankranjan07@gmail.com>
2025-04-05 18:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (40.31%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,19 +1,12 @@
 func twoSum(nums []int, target int) []int {
-    var ans []int
     mpp := make(map[int]int)
 
     for i:=0; i<len(nums); i++ {
         toFind := target - nums[i]
-
-        value, ok := mpp[toFind]
-
-        if ok {
-            ans = append(ans, value, i)
-            return ans
+        if value, ok := mpp[toFind]; ok {
+            return []int{value, i}
         }
         mpp[nums[i]] = i
     }
-
-    ans = append(ans, -1, -1)
-    return ans
+    return []int{-1,-1}
 }

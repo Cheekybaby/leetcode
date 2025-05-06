b283e2662c52762ec6e224a6fbb956dee4c885de
Shashank Ranjan <shashankranjan07@gmail.com>
2025-05-07 00:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.78%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,57 +1,23 @@
 func topKFrequent(nums []int, k int) []int {
-    minHeap :=  &MinHeap{}
-    heap.Init(minHeap)
-
+    bucket := make([][]int, len(nums)+1)
     freq := make(map[int]int)
-
+    var ans []int
     for _, num := range nums {
         freq[num]++
     }
 
-    for key, f := range freq {
-        heap.Push(minHeap, Pair{key, f})
-        if minHeap.Len() > k {
-            heap.Pop(minHeap)
+    for k, v := range freq {
+        bucket[v] = append(bucket[v], k)
+    }
+
+
+    for i := len(bucket)-1; i>=0 && k > 0 ; i-- {
+        buck := bucket[i]
+        for j:=0; j<len(buck); j++ {
+            ans = append(ans, buck[j])
+            k--
         }
     }
 
-    var ans []int
-
-    for minHeap.Len() > 0 {
-        x := heap.Pop(minHeap).(Pair)
-        ans = append(ans, x.num)
-    }
-
     return ans
-}
-
-type Pair struct {
-    num int
-    fre int
-}
-
-type MinHeap []Pair
-
-func (h MinHeap) Len() int {
-    return len(h)
-}
-
-func (h MinHeap) Less(i, j int) bool {
-    return h[i].fre < h[j].fre
-}
-
-func (h MinHeap) Swap(i, j int) {
-    h[i], h[j] = h[j], h[i]
-}
-
-func (h *MinHeap) Push(x interface{}) {
-    *h = append(*h, x.(Pair))
-}
-
-func (h *MinHeap) Pop() interface{} {
-    old := *h
-    n := len(old)
-    x := old[n-1]
-    *h = old[0:n-1]
-    return x
 }

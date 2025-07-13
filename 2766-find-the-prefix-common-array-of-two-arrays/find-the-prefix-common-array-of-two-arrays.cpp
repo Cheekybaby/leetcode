38844d225fe3cf4281c0fd749655e00f2751d370
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-13 07:48:56 +0530

    Time: 21 ms (20.83%) | Memory: 90.6 MB (21.48%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -2,31 +2,23 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
-        unordered_set<int> st;
-        vector<int> ans(n);
+        vector<int> ans(n, 0);
 
-        ans[0] = (A[0] == B[0]) ? 1 : 0;
-        ans[n-1] = n;
-        st.insert(A[0]);
-        st.insert(B[0]);
-        
-        for (int i=1; i<n-1; i+=1){
-            int x = A[i];
-            int y = B[i];
-
+        unordered_set<int> as, bs;
+        for(int i=0; i<n; i++){
             int cnt = 0;
-            if (x == y) {
-                cnt += 1;
+            if (A[i] == B[i]) cnt += 1;
+            else {
+                if (as.count(B[i])) cnt += 1;
+                if (bs.count(A[i])) cnt += 1;
             }
+            if (i > 0) cnt += ans[i-1];
+            
+            ans[i] = cnt;
 
-            if (st.count(x) == 1) cnt += 1;
-            if (st.count(y) == 1) cnt += 1;
-            st.insert(x);
-            st.insert(y);
-
-            ans[i] = ans[i-1] + cnt;
+            as.insert(A[i]);
+            bs.insert(B[i]);
         }
-
         return ans;
     }
 };

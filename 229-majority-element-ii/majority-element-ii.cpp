ee861007689783488038c1dea70c51693c52c31f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-16 01:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (58.23%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> majority;
-        unordered_map<int,int> m;
-        for(int i=0; i<nums.size(); i++){
-            m[nums[i]]++;
+        int cnt1 = 0, cnt2 = 0;
+        int cand1 = -1, cand2 = -1;
+        for(auto &num:nums){
+            if (num == cand1) cnt1++;
+            else if (num == cand2) cnt2++;
+            else if (cnt1 == 0) {
+                cand1 = num;
+                cnt1++;
+            }
+            else if (cnt2 == 0) {
+                cand2 = num;
+                cnt2++;
+            }
+            else {
+                cnt1--; cnt2--;
+            }
         }
-        int val = nums.size()/3;
-        for(auto &it:m){
-            if (it.second > val) majority.push_back(it.first);
+        int threshold = nums.size()/3;
+        cnt1 = 0, cnt2 = 0;
+        for(auto &num:nums){
+            if (num == cand1) cnt1++;
+            else if (num == cand2) cnt2++;
         }
-
-        return majority;
+        vector<int> ans;
+        if (cnt1 > threshold) ans.push_back(cand1);
+        if (cnt2 > threshold) ans.push_back(cand2);
+        return ans;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });

799b444f0d8ec93ff024d59fff02814d9c3fa5a1
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-16 00:52:53 +0530

    Time: 7 ms (42.91%) | Memory: 26.2 MB (36.77%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cand1 = -1, vote1 = 0;
-        int cand2 = -1, vote2 = 0;
-
-        for (int i=0; i<nums.size(); i+=1){
-            if (nums[i] == cand1){
-                vote1 += 1;
-            } else if (nums[i] == cand2){
-                vote2 += 1;
-            } else if (vote1 == 0){
-                cand1 = nums[i];
-                vote1 += 1;
-            } else if (vote2 == 0){
-                cand2 = nums[i];
-                vote2 += 1;
-            } else {
-                vote1 -= 1;
-                vote2 -= 1;
-            }
+        vector<int> majority;
+        unordered_map<int,int> m;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        int val = nums.size()/3;
+        for(auto &it:m){
+            if (it.second > val) majority.push_back(it.first);
         }
 
-        vote1 = 0, vote2 = 0;
-
-        for (auto it:nums){
-            if (it == cand1){
-                vote1 += 1;
-            } else if (it == cand2){
-                vote2 += 1;
-            }
-        }
-        vector<int> ans;
-        if (vote1 > nums.size()/3){
-            ans.push_back(cand1);
-        }
-        if (vote2 > nums.size()/3){
-            ans.push_back(cand2);
-        }
-
-        return ans;
+        return majority;
     }
 };

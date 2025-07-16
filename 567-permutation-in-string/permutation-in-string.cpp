fc2744e6082a12fc34533e7e4714d0506cf52ac1
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-17 01:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (86.58%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,29 +1,33 @@
 class Solution {
 private:
-    bool isValid(vector<int> &temp){
-        for(auto &t:temp){
-            if (t != 0) return false; 
+    bool isEqual(int a[26], int b[26]){
+        for(int i=0; i<26; i++){
+            if (a[i] != b[i]) return false;
         }
         return true;
     }
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> str(26, 0);
+        if (s1.length() > s2.length()) return false;
+
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+
+        for(auto ch:s1){
+            freq1[ch-'a']++;
+        }
         for(int i=0; i<s1.length(); i++){
-            str[s1[i]-'a']++;
+            freq2[s2[i]-'a']++;
         }
 
-        for(int i=0; i<s2.length(); i++){
-            int ch = s2[i] - 'a';
-            if (str[ch] != 0){
-                int j = i;
-                vector<int> temp_freq = str;
-                while (j < s2.length() && (temp_freq[s2[j] - 'a'] > 0)){
-                    temp_freq[s2[j]-'a']--;
-                    j+=1;
-                }
-                if (isValid(temp_freq)) return true;
-            }
+        if (isEqual(freq1, freq2)) return true;
+        int j = 0;
+        for(int i=s1.length(); i<s2.length(); i++){
+            freq2[s2[i]-'a']++;
+            freq2[s2[j]-'a']--;
+
+            if (isEqual(freq1, freq2)) return true;
+            j++;
         }
         return false;
     }

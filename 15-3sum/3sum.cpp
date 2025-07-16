eeffe4ca6f106fe53c7e8546dd196a561cc4cfda
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-16 16:38:09 +0530

    Time: 41 ms (90.40%) | Memory: 28.9 MB (90.17%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
+        vector<vector<int>> triplets;
+        for(int i=0; i<nums.size(); i++){
+            if (i > 0 && nums[i] == nums[i-1]) continue;
 
-        for(int i=0; i<nums.size(); i+=1){
-            if (i != 0 && nums[i] == nums[i-1]) continue;
-            int l = i+1, r = nums.size()-1;
-            while (l < r){
-                int sum = nums[i] + nums[l] + nums[r];
-
+            int j = i+1, k = nums.size()-1;
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0){
-                    ans.push_back({nums[i], nums[l], nums[r]});
-
-                    l+=1;
-                    r-=1;
-
-                    while(l<r && nums[l] == nums[l-1]) l+=1;
-                    while(l<r && nums[r] == nums[r+1]) r-=1;
+                    triplets.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 } else if (sum > 0){
-                    r-=1;
+                    k--;
                 } else {
-                    l+=1;
+                    j++;
                 }
             }
         }
-        return ans;
+
+        return triplets;
     }
 };

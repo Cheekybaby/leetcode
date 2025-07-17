930ed4fc67f420fea3b389ef45065b5b9cd9bfa2
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-18 00:09:01 +0530

    Time: 10 ms (3.89%) | Memory: 12.7 MB (42.08%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,38 +1,44 @@
 class Solution {
-public:
-    int calPoints(vector<string>& operations) {
-        stack<int> st;
-        int i = 0;
-        while (i<operations.size()){
-            string c = operations[i];
-
-            if (c == "+"){
-                if (st.size()>1){
-                    int a, b;
-                    a = st.top();
-                    st.pop();
-                    b = st.top();
-                    st.push(a);
-                    st.push(a+b);
-                }
-            } else if (c == "D"){
-                if (!st.empty()){
-                    st.push(2*(st.top()));
-                }
-            } else if (c == "C"){
-                if (!st.empty()) st.pop();
-            } else {
-                int x = stoi(c);
-                st.push(x);
-            }
-            i+=1;
+private:
+    int getNumber(string &s){
+        bool neg = false;
+        
+        if (s[0] == '-'){
+            neg = true;
+            s = s.substr(1);
         }
-        int ans = 0;
-        while (!st.empty()){
-            ans += st.top();
+        
+        int n = stoi(s);
+        
+        return (neg) ? -1*n : n;
+    }
+public:
+    int calPoints(vector<string>& ops) {
+        stack<int> st;
+        for(string &it:ops){
+            if (it == "+"){
+                int a = st.top();
+                st.pop();
+                int sum = a + st.top();
+                st.push(a);
+                st.push(sum);
+            } else if (it == "D"){
+                int a = st.top() * 2;
+                st.push(a);
+            } else if (it == "C"){
+                st.pop();
+            } else {
+                int num = getNumber(it);
+                cout << num << endl;
+                st.push(num);
+            }
+        }
+        int total_score = 0;
+        while(!st.empty()){
+            total_score += st.top();
             st.pop();
         }
 
-        return ans;
+        return total_score;
     }
 };

501e4658d032c00472e857fb63818fb9c55c31fe
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-18 00:15:41 +0530

    Time: 3 ms (7.04%) | Memory: 8.6 MB (96.43%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,30 +1,31 @@
 class Solution {
+private:
+    bool isOpen(char c){
+        if (c == '(' || c == '[' || c == '{') return true;
+        return false;
+    }
+    bool isValidPair(char c, char t){
+        if (c == '(' && t == ')') return true;
+        if (c == '{' && t == '}') return true;
+        if (c == '[' && t == ']') return true;
+        return false;
+    }
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for (int i=0; i<s.length(); i++){
-            if (
-                s[i] == '(' ||
-                s[i] == '{' ||
-                s[i] == '['
-            ) {
-                st.push(s[i]);
+        for(char c:s){
+            if (isOpen(c)){
+                st.push(c);
             } else {
                 if (st.empty()) return false;
-                char c = st.top();
-                if ((c == '(' && s[i] == ')') ||
-                    (c == '{' && s[i] == '}') ||
-                    (c == '[' && s[i] == ']')
-                ) {
+                if (isValidPair(st.top(), c)){
                     st.pop();
                 } else {
                     return false;
                 }
             }
         }
-        if (!st.empty()) return false;
-
-        return true;
+        if (st.empty()) return true;
+        return false;
     }
 };

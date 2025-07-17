8c74647b78871e7b681737944cc4370c571e8d74
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-18 00:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (19.69%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,16 +1,13 @@
+static const unordered_map<char, char> bracket_pairs{
+    {')', '('}, {']', '['}, {'}', '{'}};
 class Solution {
-private:
-    unordered_map<char,char> bracket_pairs {
-        {')', '('},
-        {']', '['},
-        {'}', '{'}
-    };
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(char &c:s){
-            if (bracket_pairs.count(c)){
-                if (st.empty() || bracket_pairs[c] != st.top()) return false;
+        for (char& c : s) {
+            if (bracket_pairs.count(c)) {
+                if (st.empty() || bracket_pairs.at(c) != st.top())
+                    return false;
                 st.pop();
             } else {
                 st.push(c);

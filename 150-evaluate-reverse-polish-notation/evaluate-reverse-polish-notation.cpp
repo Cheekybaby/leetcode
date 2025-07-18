9d9655b5ef73d0a98ae23cff760985f37cbddb91
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-18 12:53:34 +0530

    Time: 3 ms (38.88%) | Memory: 18.3 MB (8.51%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,44 +1,40 @@
 class Solution {
+private:
+    pair<int,int> operands(stack<int> &st){
+        int a = st.top();
+        st.pop();
+        int b = st.top();
+        st.pop();
+        return {a, b};
+    }
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        int i=0;
-        while (i<tokens.size()){
-            string s = tokens[i];
-            if (st.size() > 1){
-                if (s == "+"){
-                    int a = st.top();
-                    st.pop();
-                    int b = st.top();
-                    st.pop();
-                    st.push(a+b);
-                } else if (s == "-"){
-                    int a = st.top();
-                    st.pop();
-                    int b = st.top();
-                    st.pop();
-                    st.push(b-a);
-                } else if (s == "*"){
-                    int a = st.top();
-                    st.pop();
-                    int b = st.top();
-                    st.pop();
-                    st.push(b*a);
-                } else if (s == "/"){
-                    int a = st.top();
-                    st.pop();
-                    int b = st.top();
-                    st.pop();
-                    st.push(b/a);
-                } else {
-                    int x = stoi(s);
-                    st.push(x);
+        for(auto &s:tokens){
+            if (s == "+"){
+                if (st.size() >= 2){
+                    auto [a, b] = operands(st);
+                    st.push(b + a);
+                }
+            } else if (s == "-"){
+                if (st.size() >= 2){
+                    auto [a, b] = operands(st);
+                    st.push(b - a);
+                }
+            } else if (s == "*"){
+                if (st.size() >= 2){
+                    auto [a, b] = operands(st);
+                    st.push(b * a);
+                }
+            } else if (s == "/"){
+                if (st.size() >= 2){
+                    auto [a, b] = operands(st);
+                    st.push(b / a);
                 }
             } else {
                 int x = stoi(s);
                 st.push(x);
             }
-            i+=1;
         }
         return st.top();
     }

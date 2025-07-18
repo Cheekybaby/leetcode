ec3b661e0f0cc710bd689b81c60a2092f4c5b9f9
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-18 13:03:40 +0530

    Time: 3 ms (72.18%) | Memory: 13 MB (83.53%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,28 +1,27 @@
 class Solution {
+private:
+    void possibleCombinations(int open, int close, int n, vector<string> &combos, string &st){
+        if (open == n && close == n){
+            combos.push_back(st);
+            return;
+        }
+
+        if (open < n){
+            st += "(";
+            possibleCombinations(open+1, close, n, combos, st);
+            st.pop_back();
+        }
+        if (close < open){
+            st += ")";
+            possibleCombinations(open, close+1, n, combos, st);
+            st.pop_back();
+        }
+    }
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        string temp;
-        solve(n, 0, 0, temp, ans);
-        return ans;
-    }
-
-private:
-    void solve(int n, int open, int close, string &temp, vector<string> &ans) {
-        if (open == n && close == n) {
-            ans.push_back(temp);
-            return ;
-        }
-
-        if (open < n) {
-            temp += "(";
-            solve(n, open+1, close, temp, ans);
-            temp.pop_back();
-        }
-        if (close < open && close < n) {
-            temp += ")";
-            solve(n, open, close+1, temp, ans);
-            temp.pop_back();
-        }
+        vector<string> combos;
+        string st;
+        possibleCombinations(0, 0, n, combos, st);
+        return combos;
     }
 };

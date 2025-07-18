6d0ebad772fc38fdd88541278555a46c25ec2ea0
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-18 22:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (38.41%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
-    // int ones(int n){
-    //     int cnt = 0;
-    //     while(n > 0){
-    //         if (n%2 == 1) cnt += 1;
-    //         n/=2;
-    //     }
-
-    //     return cnt;
-    // }
-    int ones(int n, vector<int> &v){
-        if (n == 0) return 0;
-        if (v[n>>1] != -1) return (n&1) + v[n>>1];
-        return ones(n>>1, v);
-    }
     vector<int> countBits(int n) {
-        vector<int> ans(n+1, -1);
-        ans[0] = 0;
-        for(int i=1; i<=n; i+=1){
-            // ans[i] = __builtin_popcount(i); // Using builtin method
-            // ans[i] = ones(i); O(nlogn)
-            ans[i] = ones(i, ans);
-
+        vector<int> bits(n+1, 0);
+        for(int i=1; i<=n; i++){
+            bits[i] = bits[i>>1] + (i & 1);
         }
-        return ans;
+        return bits;
     }
 };

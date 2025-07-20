f06cf7937873bf51db36b6cc071e9aa495f0007f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-20 15:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (99.96%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,35 +1,23 @@
-/*
-- Maximum possible sum of a non-empty subarray of given array
-- We create two variables for global maxima and global minima
-- Also two variables for the current / local maxima and minima
-
-- At every step we check if the current maximum is greater or the current element is greater.
-- We update the gloabal maximum and minimum accordingly.
-- If the global maximum is greater than zero then we know there exists atleast one positive element. So we need to check if given global maximum if greater than the sum - global minimum. Whatever is greater is the answer.
-- While for negative global maximum, it is the answer as it is clear that there is no negative element in the array. 
-*/
-
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int globalMax = nums[0], globalMin = nums[0];
-        int currMax = 0, currMin = 0;
+        int global_max = nums[0], global_min = nums[0];
+        int curr_max = 0, curr_min = 0;
 
         int sum = 0;
-
-        for (int i=0; i<nums.size(); i+=1){
-            currMax = max(currMax + nums[i], nums[i]);
-            currMin = min(currMin + nums[i], nums[i]);
+        for(int i=0; i<nums.size(); i++){
+            curr_max = max(curr_max+nums[i], nums[i]);
+            curr_min = min(curr_min+nums[i], nums[i]);
 
             sum += nums[i];
 
-            globalMax = max(globalMax, currMax);
-            globalMin = min(globalMin, currMin);
+            global_max = max(global_max, curr_max);
+            global_min = min(global_min, curr_min);
         }
 
-        if (globalMax > 0){
-            return max(globalMax, sum - globalMin);
-        } 
-        return globalMax;
+        if (global_max > 0){
+            return max(global_max, sum - global_min);
+        }
+        return global_max;
     }
 };

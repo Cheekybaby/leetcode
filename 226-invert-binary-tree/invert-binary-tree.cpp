a8a977fda203cbcbecd17b1f586065c888260b7e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-21 14:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (59.25%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -16,13 +16,12 @@ public:
 
         queue<TreeNode*> q;
         q.push(root);
-
         while(!q.empty()){
             int n = q.size();
             for(int i=0; i<n; i++){
                 TreeNode *curr = q.front();
                 q.pop();
-                
+
                 TreeNode *temp = curr->left;
                 curr->left = curr->right;
                 curr->right = temp;
@@ -31,7 +30,6 @@ public:
                 if (curr->right) q.push(curr->right);
             }
         }
-
         return root;
     }
 };

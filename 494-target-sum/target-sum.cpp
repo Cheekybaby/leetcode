20ff09dd86c0bc941272b7e1c4c8ecb80844dfca
Shashank Ranjan <shashankranjan07@gmail.com>
2025-07-26 00:33:31 +0530

    Time: 15 ms (53.66%) | Memory: 25.8 MB (28.85%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,30 +1,23 @@
 class Solution {
-private:
-    int OFFSET = 1000;
-    int dp[22][2004];
 public:
-    Solution(){
-        memset(dp, -1, sizeof(dp));
-    }
-    int solve(vector<int> &nums, int target, int n, int sum){
-        if (n < 0){
-            if (sum == target) return 1;
-            return 0;
-        }
-        
-        if (dp[n][OFFSET+sum] != -1) return dp[n][OFFSET+sum];
-
-        int add = solve(nums, target, n-1, sum + nums[n]);
-        int sub = solve(nums, target, n-1, sum - nums[n]);
-
-        dp[n][OFFSET+sum] = add + sub;
-        
-        return dp[n][OFFSET+sum];
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        int n = nums.size()-1;
-        int cnt = solve(nums, target, n, sum);
-        return cnt;
+        int n = nums.size();
+        int max_sum = accumulate(nums.begin(), nums.end(), 0);
+        int min_sum = -1 * max_sum;
+
+        vector<vector<int>> dp(n, vector<int> ((2*max_sum)+1, 0));
+        dp[0][nums[0] + max_sum] = 1;
+        dp[0][-nums[0] + max_sum] += 1;
+
+        for(int i=1; i<n; i++){
+            for(int j=min_sum; j<=max_sum; j++){
+                if (dp[i-1][j+max_sum] > 0){
+                    dp[i][j+nums[i]+max_sum] += dp[i-1][j+max_sum];
+                    dp[i][j-nums[i]+max_sum] += dp[i-1][j+max_sum];
+                }
+            }
+        }
+
+        return abs(target) > max_sum ? 0 : dp[n-1][target+max_sum];
     }
 };

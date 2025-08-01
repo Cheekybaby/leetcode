5129ebd691c68e812c5c2aafef43bd8557f8bec0
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-01 21:36:24 +0530

    Time: 31 ms (25.72%) | Memory: 46.2 MB (11.59%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,23 +1,27 @@
+#define ull unsigned long long
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, int n, vector<vector<int>> &dp) {
-        if (amount < 0) return 0;
-        if (amount == 0) return 1;
-        if (n < 0) return 0;
-        if (dp[n][amount] != -1) return dp[n][amount];
-
-        int not_take = solve(coins, amount, n-1,  dp);
-        int take = 0;
-        if (amount >= coins[n]){
-            take = solve(coins, amount-coins[n], n, dp);
-        }
-
-        return dp[n][amount] = take + not_take;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        int ans = solve(coins, amount, n-1, dp);
-        return ans;
+        vector<vector<ull>> dp(n, vector<ull>(amount + 1, 0));
+
+        for (int i = 0; i < n; i++) dp[i][0] = 1;
+        
+        for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0) dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                ull not_take = dp[i - 1][j];
+                ull take = 0;
+                if (j >= coins[i]) {
+                    take = dp[i][j - coins[i]];
+                }
+                dp[i][j] = take + not_take;
+            }
+        }
+
+        return dp[n - 1][amount];
     }
 };

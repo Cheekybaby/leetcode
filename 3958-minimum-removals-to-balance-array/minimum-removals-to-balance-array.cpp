358f3f20e7a3a62b9a95306690be9ab47d89d387
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-04 01:35:08 +0530

    Time: 35 ms (11.11%) | Memory: 105.1 MB (0.00%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -1,25 +1,13 @@
-#define ll unsigned long long int
 class Solution {
 public:
     int minRemoval(vector<int>& nums, int k) {
-        int n = nums.size();
-        int ans = n;
-        sort(nums.begin(), nums.end());
-        for(auto &it:nums) cout << it << " ";
-        for(int i=0; i<n; i++){
-            int curr_val = nums[i];
-            ll max_possible = (ll)curr_val * k;
-            if (max_possible >= nums[n-1]){
-                // Everything is included from here onwards, and since the array is sorted, we don't need to go any further with out iteration.
-                int remove = i;
-                ans = min(ans, remove);
-                break;
-            }
-            int j = upper_bound(nums.begin(), nums.end(), max_possible) - nums.begin();
-            int remove = n - (j - i);
-            ans = min(ans, remove);
+        int res = INT_MAX, n = nums.size();
+        sort(begin(nums), end(nums));
+        for(int i = 0, j = 0; i < n; i++) {
+            if(i and nums[i] == nums[i-1]) continue;
+            while(j < n and (long long)nums[i] * k >= (long long)nums[j]) j++;
+            res = min(res, i + n - j);
         }
-
-        return ans;
+        return res;
     }
 };

7eb6c9567dccc8e971018af00bf7fe6683463a2c
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-06 10:25:34 +0530

    Time: 472 ms (30.42%) | Memory: 351.6 MB (20.92%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> graph;
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
-        
-        unordered_set<int> visited;
-        return dfs(source, destination, graph, visited);
-    }
-    
-    bool dfs(int node, int destination, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited) {
-        if (node == destination) {
+    bool bfs(unordered_map<int, vector<int>>& adj,
+             unordered_map<int, bool>& visited, int source, int destination) {
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        if (visited[destination] == true)
             return true;
-        }
-        visited.insert(node);
-        for (int neighbor : graph[node]) {
-            if (visited.find(neighbor) == visited.end()) {
-                if (dfs(neighbor, destination, graph, visited)) {
-                    return true;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (auto& i : adj[u]) {
+                if (visited[i] == false) {
+                    visited[i] = true;
+                    q.push(i);
+
+                    if (visited[destination] == true)
+                        return true;
                 }
             }
         }
         return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source,
+                   int destination) {
+        unordered_map<int, vector<int>> adj;
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        unordered_map<int, bool> visited;
+        return bfs(adj, visited, source, destination);
     }
 };

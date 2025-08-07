358879430936a80ce7f206086c232e32e99f2fb3
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-07 15:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (82.60%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,vector<int>> adj;
+        vector<int> indegree(n+1, 0);
+        vector<int> outdegree(n+1, 0);
         for(int i=0; i<trust.size(); i++){
             int u = trust[i][0];
             int v = trust[i][1];
 
-            adj[u].push_back(v);
-        }
-        vector<int> indegree(n+1, 0);
-        for(int i=1; i<=n; i++){
-            for(auto &v:adj[i]){
-                indegree[v]++;
-            }
+            outdegree[u]++;
+            indegree[v]++;
         }
         for(int i=1; i<=n; i++){
-            int x = i;
-            int y = indegree[i];
-
-            if (adj[x].size() == 0 && y == n-1) return x;
+            if (outdegree[i] == 0 && indegree[i] == n-1) return i;
         }
-
         return -1;
     }
 };

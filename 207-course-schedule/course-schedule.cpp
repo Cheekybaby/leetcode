05f1679f655394b63a87ce535aed798bcb3444e0
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-07 11:34:31 +0530

    Time: 7 ms (48.83%) | Memory: 20 MB (47.99%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,48 +1,34 @@
+// Check if there is a cycle or not. If there is a cycle then we can't take all the courses
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size() == 0) return true;
-        if(numCourses == 1) return !(prerequisites.size() == 1);
-        vector<vector<int>> adj = createAdj(prerequisites, numCourses);
-        int n = adj.size();
-        vector<int> inDegree(n, 0);
-        for(int i=0; i<n; i++){
-            for(auto &it:adj[i]){
-                inDegree[it]++;
-            }
+    bool dfs(unordered_map<int,vector<int>> &adj, vector<bool> &visited, int u, vector<bool> &inRecursion){
+        visited[u] = true;
+        inRecursion[u] = true;
+        for(auto &v:adj[u]){
+            if (visited[v] == false){
+                if (dfs(adj, visited, v, inRecursion)) return true;
+            } else if (inRecursion[v] == true) return true;
         }
-
-        queue<int> q;
-        for(int i=0; i<n; i++){
-            if(inDegree[i] == 0){
-                q.push(i);
-            }
-        }
-
-        vector<int> topo_order;
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            topo_order.push_back(curr);
-
-            for(auto &it:adj[curr]){
-                if(--inDegree[it] == 0){
-                    q.push(it);
-                }
-            }
-        }
-
-        return (n == topo_order.size());
+        inRecursion[u] = false;
+        return false;
     }
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        unordered_map<int,vector<int>> adj;
 
-private:
-    vector<vector<int>> createAdj(vector<vector<int>> &prerequisites, int n){
-        vector<vector<int>> adj(n);
-        int size = prerequisites.size();
-        for(int i=0; i<size; i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for(int i=0; i<pre.size(); i++){
+            int u = pre[i][1];
+            int v = pre[i][0];
+
+            adj[u].push_back(v);
         }
 
-        return adj;
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
+        for(int i=0; i<numCourses; i++){
+            if (visited[i] == false && dfs(adj, visited, i, inRecursion)){
+                return false;
+            }
+        }
+        return true;
     }
 };

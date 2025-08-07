97402b106128c78d1867458e5a2c911173be8897
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-07 16:42:01 +0530

    Time: 246 ms (0.84%) | Memory: 294.5 MB (5.65%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -6,22 +6,58 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-public:
-    bool isMirror(TreeNode* p, TreeNode* q) {
-        if (p == nullptr && q == nullptr)
-            return true;
-        if (p == nullptr || q == nullptr)
-            return false;
-
-        return (p->val == q->val && isMirror(p->left, q->right) &&
-                isMirror(p->right, q->left));
+private:
+    bool isPalindrome(vector<int> &val){
+        int i=0, j=val.size()-1;
+        while(i < j){
+            if (val[i] != val[j]) return false;
+            i++; j--;
+        }
+        return true;
     }
+
+    bool allDummy(vector<int> &val){
+        for(auto &it:val){
+            if (it != -101) return false;
+        }
+        return true;
+    }
+public:
     bool isSymmetric(TreeNode* root) {
-        return isMirror(root->left, root->right);
+        TreeNode *dummy = new TreeNode(-101);
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> val(n);
+            for(int i=0; i<n; i++){
+                TreeNode *node = q.front();
+                q.pop();
+
+                val[i] = node->val;
+
+                if (node->left){
+                    q.push(node->left);
+                } else {
+                    q.push(dummy);
+                }
+
+                if (node->right){
+                    q.push(node->right);
+                } else {
+                    q.push(dummy);
+                }
+            }
+            if (!isPalindrome(val)) return false;
+            
+            if (allDummy(val)) break;
+        }
+
+        return true;
     }
 };

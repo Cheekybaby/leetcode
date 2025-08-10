a67ef773b8e7290f1a5bb1785bd188ed4e809456
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-10 18:00:38 +0530

    Time: 39 ms (56.84%) | Memory: 74.9 MB (72.86%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> mpp;
         int i=0, j=0;
-        int ans = 0, total = 0;
-        while (j<fruits.size()){
-            mpp[fruits[j]]+=1;
-            total+=1;
-            while (mpp.size()>2){
-                mpp[fruits[i]]-=1;
-                total-=1;
-                if (mpp[fruits[i]] == 0) mpp.erase(fruits[i]);
-                i+=1;
+        unordered_map<int,int> basket;
+        int max_fruit = 0, sum = 0;
+        while(j < fruits.size()){
+            basket[fruits[j]]++;
+            
+            sum += 1;
+            
+            while(i < j && basket.size() > 2){
+                basket[fruits[i]]--;
+                sum -= 1;
+                if (basket[fruits[i]] == 0) basket.erase(fruits[i]);
+
+                i++;
             }
 
-            ans = max(ans, total);
-            j+=1;
+            max_fruit = max(max_fruit, sum);
+            j++;
         }
 
-        return ans;
+        return max_fruit;
     }
 };
+
+
+/*
+We need to consider every subarray with only two kinds of fruits.
+So we start and start putting the fruits in the basket.
+Whenever we see encounter a new fruit, if only one basket is being used, then we just keep it and move on, but if no basket is empty, then we start taking out the fruits we started from until there are only two types left, then we move on again.
+
+So whenever we encounter a third type, we update the maximum fruit seen till now.
+
+*/

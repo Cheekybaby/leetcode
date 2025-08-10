83a3602863941c95ac361a09962baf770441fb59
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-10 23:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (24.63%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,36 @@
 class Solution {
-public:
-    void solve(vector<int> &nums, vector<vector<int>> &ans, unordered_set<int> &st, vector<int> &temp, int index){
-        if (index == nums.size()){
+private:
+    void solve(vector<int> &nums, unordered_set<int> &st, vector<vector<int>> &ans, vector<int> &temp, int idx){
+        if (idx == nums.size()){
             ans.push_back(temp);
-            return ;
+            return;
         }
 
         for(auto &it:nums){
             if (st.count(it) == 0){
-                temp.push_back(it);
                 st.insert(it);
-                solve(nums, ans, st, temp, index+1);
-                temp.pop_back();
+                temp.push_back(it);
+                solve(nums, st, ans, temp, idx+1);
                 st.erase(it);
+                temp.pop_back();
             }
         }
     }
+public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
         unordered_set<int> st;
         vector<int> temp;
+        vector<vector<int>> ans;
 
-        solve(nums, ans, st, temp, 0);
+        solve(nums, st, ans, temp, 0);
 
         return ans;
     }
 };
+
+/*
+    - We need to find all the permutation of the given array nums
+    - The length of nums is <= 8, so we can go for backtracking kind of solution. 
+    - How do we use the backtracking?
+        -
+*/

b99fc435df7101b279a69f60db3f6344c1efb3dd
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-16 11:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.40%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,10 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int l = lower_bound(nums, target);
-        int r = upper_bound(nums, target);
-        return {l, r};
-    }
+        int i = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int j = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
 
-private:
-    int lower_bound(vector<int> &nums, int target){
-        int l = 0, r = nums.size()-1;
-        while(l <= r){
-            int mid = midpoint(l, r);
-            if (nums[mid] == target){
-                if (mid == 0) return mid;
-                else {
-                    if (nums[mid-1] != target) return mid;
-                    else r = mid - 1;
-                }
-            } else if (nums[mid] > target){
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return -1;
-    }
-    int upper_bound(vector<int> &nums, int target){
-        int l = 0, r = nums.size()-1;
-        while(l <= r){
-            int mid = midpoint(l, r);
-            if (nums[mid] == target){
-                if (mid == nums.size()-1) return mid;
-                else {
-                    if (nums[mid+1] != target) return mid;
-                    else l = mid + 1;
-                }
-            } else if (nums[mid] > target){
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return -1;
+        if (i == j) return {-1,-1};
+        return {i, j-1};
     }
 };

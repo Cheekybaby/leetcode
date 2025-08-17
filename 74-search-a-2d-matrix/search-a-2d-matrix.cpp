366db81e161379d24f7fd9b8bf27964c3606c77b
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-17 12:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.11%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size(); 
-        int col = matrix[0].size();
-        // do a binary search on the last column to find the correct row to look for
-        int l = 0 , r = row-1;
-        while (l <= r){
-            int mid = l + (r - l)/2;
-            if (matrix[mid][col-1] == target) return true;
-            else if (matrix[mid][col-1] > target) r = mid-1;
-            else l = mid+1;
-        }
-        if (l >= row) return false;
-        // now do a binary search on that row to check for the value
-        int i = 0, j = col-1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int i = 0, j = m-1;
         while (i <= j){
-            int mid = i + (j - i) / 2;
-            if (matrix[l][mid] == target) return true;
-            else if (matrix[l][mid] > target) j = mid-1;
-            else i = mid+1;
+            int mid = midpoint(i, j);
+
+            if (matrix[mid][n-1] == target) return true;
+            else if (matrix[mid][n-1] > target) j = mid - 1;
+            else i = mid + 1;
+        }
+
+        if (i > m-1) return false;
+
+        int l = 0, r = n-1;
+        while(l <= r){
+            int mid = midpoint(l, r);
+            
+            if (matrix[i][mid] == target) return true;
+            else if (matrix[i][mid] > target) r = mid - 1;
+            else l = mid + 1;
         }
 
         return false;

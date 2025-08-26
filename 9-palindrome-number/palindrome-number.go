67287b9939b920269b3894a6e4936475f73218d3
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-26 23:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (54.83%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.go
@@ -0,0 +1,18 @@
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+    y := x
+    rev := 0
+    for y > 0 {
+        rem := y % 10
+        rev = rev * 10 + rem
+
+        y/=10
+    }
+
+    if rev == x {
+        return true
+    }
+    return false
+}

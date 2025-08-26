33f9ef9ad96fcbcba7a3a5e1902bff558d74e1ca
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-26 23:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (60.18%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.go
+++ 2-add-two-numbers/add-two-numbers.go
@@ -6,38 +6,24 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	// Create new head
 	newHead := &ListNode{Val: 0, Next: nil}
 	temp := newHead
 
-	carry := 0 // Takes care of the carry along the way
+	carry := 0 
 
 	for l1 != nil || l2 != nil {
 		var a, b int
-		if l1 != nil {
-			a = l1.Val
-		} else {
-			a = 0
-		}
-
-		if l2 != nil {
-			b = l2.Val
-		} else {
-			b = 0
-		}
+		a = findVal(l1)
+        b = findVal(l2)
 
 		sum := a + b + carry
 		carry = sum / 10
-		// Now say temp is looking here
+
 		temp.Next = &ListNode{Val: sum % 10, Next: nil}
 		temp = temp.Next
 
-        if l1 != nil {
-            l1 = l1.Next
-        }
-        if l2 != nil {
-            l2 = l2.Next
-        }
+        l1 = moveForward(l1)
+        l2 = moveForward(l2)
 	}
 
 	if carry > 0 {
@@ -45,4 +31,18 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 
 	return newHead.Next
+}
+
+func findVal(l *ListNode) int {
+    if l != nil {
+        return l.Val
+    }
+    return 0
+}
+
+func moveForward(l *ListNode) *ListNode {
+    if l != nil {
+        return l.Next
+    }
+    return nil
 }

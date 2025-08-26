21ece6b0d12aa4599afe0ed2906ee3f8486e973f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-26 23:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 4.3 MB (99.01%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,31 +1,34 @@
 func lengthOfLongestSubstring(s string) int {
-    var mpp [256]int
-    for i := range mpp {
-        mpp[i] = -1
-    }
-    i, j := 0, 0
-    ans := 0
-    for j=0; j<len(s);  {
-        char := (s[j])
-        val := mpp[char]
-        if val != -1 && val != j && val >= i {
-            // Reset the sliding window
-            mpp[char] = j
-            i = val + 1
-            j = i
-        } else {
-            ans = max(ans, j-i+1)
-            mpp[char] = j
-            j += 1
-        }
-    }
+	var freq [256]int
+	for k := range freq {
+		freq[k] = 0
+	}
 
-    return ans
+	i, j := 0, 0
+	maxLen := 0
+
+	for j = range s {
+		c := s[j]
+		val := freq[c]
+
+		if val > 0 {
+			// We will be resetting the window
+			for i < j && val != 0 {
+				freq[s[i]]--  // We remove the character on i from the map
+				val = freq[c] // See the value of current character in the map
+				i++           // Update the left pointer
+			}
+		}
+		maxLen = max(maxLen, j-i+1)
+		freq[c]++
+	}
+
+	return maxLen
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }

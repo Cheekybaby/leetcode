c4d387678ecbc9002f86c81667448067e8f8c92b
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-26 23:26:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (92.80%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.go
+++ 2-add-two-numbers/add-two-numbers.go
@@ -6,32 +6,43 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    head := &ListNode{}
-    temp := head
-    carry := 0
+	// Create new head
+	newHead := &ListNode{Val: 0, Next: nil}
+	temp := newHead
 
-    for l1 != nil || l2 != nil {
-        a, b := 0, 0
+	carry := 0 // Takes care of the carry along the way
+
+	for l1 != nil || l2 != nil {
+		var a, b int
+		if l1 != nil {
+			a = l1.Val
+		} else {
+			a = 0
+		}
+
+		if l2 != nil {
+			b = l2.Val
+		} else {
+			b = 0
+		}
+
+		sum := a + b + carry
+		carry = sum / 10
+		// Now say temp is looking here
+		temp.Next = &ListNode{Val: sum % 10, Next: nil}
+		temp = temp.Next
+
         if l1 != nil {
-            a = l1.Val
             l1 = l1.Next
         }
         if l2 != nil {
-            b = l2.Val
             l2 = l2.Next
         }
+	}
 
-        value := a + b + carry
-        carry = value / 10
+	if carry > 0 {
+		temp.Next = &ListNode{Val: carry, Next: nil}
+	}
 
-        temp.Next = &ListNode{Val:(value%10)}
-        temp = temp.Next
-    }
-
-    if carry == 1 {
-        temp.Next = &ListNode{Val: 1}
-        temp = temp.Next
-    }
-
-    return head.Next
+	return newHead.Next
 }

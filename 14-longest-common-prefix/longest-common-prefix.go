4e4488022b0bb6a511cfaefa4175346fcf13902e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-28 14:47:30 +0530

    Time: 3 ms (5.62%) | Memory: 7.4 MB (5.28%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.go
@@ -0,0 +1,20 @@
+func longestCommonPrefix(strs []string) string {
+    ans := strs[0]
+    for i:=1; i<len(strs); i++ {
+        // We check for the longest prefix in the current ans, and the current string
+        ans = newPrefix(ans, strs[i])
+    }
+
+    return ans
+}
+
+func newPrefix(s, p string) string {
+    var pref string
+    for i:=0; i < len(s) && i < len(p); i++ {
+        if s[i] != p[i] {
+            break
+        }
+        pref += string(s[i])
+    }
+    return pref
+}

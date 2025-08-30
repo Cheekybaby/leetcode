e474153ed1426e02b4a454c6d9e3f07570c8685e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-08-30 13:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 4.8 MB (26.68%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.go
+++ 10-regular-expression-matching/regular-expression-matching.go
@@ -1,47 +1,37 @@
 func solve(s, p string, i, j int, dp [][]int) bool {
-    if len(p) == j {
-        if len(s) == i {
-            return true
-        } else {
-            return false
-        }
+    if j == len(p) {
+        return i == len(s)
     }
 
     if dp[i][j] != -1 {
         return dp[i][j] == 1
     }
 
-    firstMatch := false
-    if i < len(s) && (p[j] == s[i] || p[j] == '.') {
-        firstMatch = true
-    }
+    firstMatch := (i < len(s)) && (s[i] == p[j] || p[j] == '.')
 
     if j < len(p)-1 && p[j+1] == '*' {
-        val := solve(s, p, i, j+2, dp) || (firstMatch && solve(s, p, i+1, j, dp))
+        dp[i][j] = 0
+        val := (firstMatch && solve(s, p, i+1, j, dp)) || (solve(s, p, i, j+2, dp))
         if val {
             dp[i][j] = 1
-        } else {
-            dp[i][j] = 0
         }
     } else {
-        val := firstMatch && solve(s, p, i+1, j+1, dp)
+        dp[i][j] = 0
+        val := (firstMatch && solve(s, p, i+1, j+1, dp))
         if val {
             dp[i][j] = 1
-        } else {
-            dp[i][j] = 0
         }
     }
     return dp[i][j] == 1
 }
-
 func isMatch(s string, p string) bool {
-    dp := initializeArray(len(s), len(p));
+    dp := initializeDP(len(s), len(p))
     return solve(s, p, 0, 0, dp)
 }
 
-func initializeArray(n, m int) (matrix [][]int) {
+func initializeDP(n, m int) (matrix [][]int) {
     matrix = make([][]int, n+1)
-    for i:= range matrix {
+    for i := range matrix {
         matrix[i] = make([]int, m+1)
         for j := range matrix[i] {
             matrix[i][j] = -1

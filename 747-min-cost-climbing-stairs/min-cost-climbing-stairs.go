0458ad2189a594f0f69be401253eaddca1c9fec2
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-04 15:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 4.8 MB (51.09%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.go
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.go
@@ -1,30 +1,12 @@
-func solve(cost []int, idx int, dp []int) int {
-    if idx >= len(cost) {
-        return 0
-    }
-    if idx == len(cost)-1 {
-        return cost[idx]
-    }
-
-    if dp[idx] != -1 {
-        return dp[idx]
-    }
-
-    dp[idx] = cost[idx] + min(solve(cost, idx+1, dp), solve(cost, idx+2, dp))
-    return dp[idx]
-}
-
 func minCostClimbingStairs(cost []int) int {
-    dp := initDP(len(cost))
-    return min(solve(cost, 0, dp), solve(cost, 1, dp))
-}
+    dp := make([]int, len(cost))
 
-func initDP(n int) (dp []int) {
-    dp = make([]int, n+1)
+    dp[0] = cost[0]
+    dp[1] = cost[1]
 
-    for i := range dp {
-        dp[i] = -1
+    for i := 2; i < len(cost); i++ {
+        dp[i] = cost[i] + min(dp[i-1], dp[i-2])
     }
 
-    return dp
+    return min(dp[len(cost)-1], dp[len(cost)-2])
 }

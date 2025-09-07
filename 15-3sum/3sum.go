1269bf5a8fd0481104e095e919efa8e66fe9b866
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-07 22:21:14 +0530

    Time: 19 ms (86.46%) | Memory: 9.8 MB (47.75%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,20 +1,17 @@
 func threeSum(nums []int) [][]int {
-    sort.Ints(nums)
+    sort.Ints(nums) // Now everything is ordered.
     var triplets [][]int
     for i := range nums {
         if i > 0 && nums[i] == nums[i-1] {
             continue
         }
 
-        a := nums[i]
-
-        l, r := i + 1, len(nums)-1
-
+        l, r := i+1, len(nums)-1
         for l < r {
-            sum := a + nums[l] + nums[r]
+            sum := nums[i] + nums[l] + nums[r]
 
             if sum == 0 {
-                triplets = append(triplets, []int{a, nums[l], nums[r]})
+                triplets = append(triplets, []int{nums[i], nums[l], nums[r]})
 
                 l++
                 r--

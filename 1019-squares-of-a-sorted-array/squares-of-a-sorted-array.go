5585c0d6ed6413aa565abc0071951ca076c120d5
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-07 22:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.55%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.go
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.go
@@ -1,20 +1,20 @@
 func sortedSquares(nums []int) []int {
     i, j, k := 0, len(nums)-1, len(nums)-1
-    ans := make([]int, len(nums))
+    square := make([]int, len(nums))
 
     for i <= j {
-        left := nums[i] * nums[i]
-        right := nums[j] * nums[j]
+        x := (nums[i]*nums[i])
+        y := (nums[j]*nums[j])
 
-        if left > right {
-            ans[k] = left
+        if x >= y {
+            square[k] = x
             i++
         } else {
-            ans[k] = right
+            square[k] = y
             j--
         }
         k--
     }
 
-    return ans
+    return square
 }

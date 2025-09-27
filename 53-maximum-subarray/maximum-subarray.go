da8ddc72714924a09be14c88ce0b11c674ae713c
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-27 20:16:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (62.50%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,13 +1,16 @@
 func maxSubArray(nums []int) int {
-    max_sum := math.MinInt
     sum := 0
-    for i := 0; i < len(nums); i++ {
+    max_sum := math.MinInt
+
+    for i := range nums {
         sum = max(nums[i], sum + nums[i])
+
         max_sum = max(max_sum, sum)
 
         if sum < 0 {
             sum = 0
         }
     }
+
     return max_sum
 }

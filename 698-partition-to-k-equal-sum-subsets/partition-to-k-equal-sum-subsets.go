e19714a67f30041b569128c025882c38c52007d3
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-29 15:54:52 +0530

    Time: 814 ms (33.33%) | Memory: 4.3 MB (23.53%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
@@ -0,0 +1,37 @@
+func solve(nums []int, visited []bool, k, idx, target, sum int) bool {
+    if k == 0 {
+        return true
+    }
+    if sum == target {
+        return solve(nums, visited, k-1, 0, target, 0)
+    }
+
+    for j := idx; j < len(nums); j++ {
+        if visited[j] || (sum + nums[j]) > target {
+            continue
+        }
+        visited[j] = true
+        if (solve(nums, visited, k, j+1, target, sum + nums[j])) {
+            return true
+        }
+        visited[j] = false
+    }
+    return false
+}
+
+func canPartitionKSubsets(nums []int, k int) bool {
+    sum := 0
+    for _, val := range nums {
+        sum += val
+    }
+
+    if (sum % k) != 0 {
+        return false
+    }
+    sort.Sort(sort.Reverse((sort.IntSlice(nums))))
+    target := (sum / k)
+    visited := make([]bool, len(nums))
+    fmt.Println(visited)
+    
+    return solve(nums, visited, k, 0, target, 0)
+}

825e13bc04648003db883b4208bb73518cb7d862
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-29 08:53:52 +0530

    Time: 184 ms (35.74%) | Memory: 57.4 MB (5.22%) - LeetSync

--- 494-target-sum/target-sum.go
+++ 494-target-sum/target-sum.go
@@ -1,36 +1,34 @@
-const OFFSET = 1000
-const n = 22
-func solve(nums []int, target int, sum int, idx int, dp [][]int) int {
-	if idx == len(nums) {
-		if target == sum {
-			return 1
-		}
-		return 0
-	}
+var dp = make([][]int, 22)
+const OFFSET = 10000
+func solve(nums []int, target, sum, idx int) int {
+    if idx >= len(nums) {
+        if target == sum {
+            return 1
+        }
+        return 0
+    }
 
-	if dp[idx][OFFSET+sum] != -1 {
-		return dp[idx][OFFSET+sum]
-	}
+    if dp[idx][OFFSET+sum] != -1 {
+        return dp[idx][OFFSET+sum]
+    }
 
-	add := solve(nums, target, sum+nums[idx], idx+1, dp)
-    sub := solve(nums, target, sum-nums[idx], idx+1, dp)
+    add := solve(nums, target, sum + nums[idx], idx+1)
+    sub := solve(nums, target, sum - nums[idx], idx+1)
 
-	dp[idx][OFFSET+sum] = add + sub
+    dp[idx][OFFSET+sum] = add + sub
     return dp[idx][OFFSET+sum]
 }
 
 func findTargetSumWays(nums []int, target int) int {
-	dp := initDP()
-	return solve(nums, target, 0, 0, dp)
+    initDP()
+    return solve(nums, target, 0, 0)
 }
 
-func initDP() (dp [][]int) {
-	dp = make([][]int, n)
-	for i := range dp {
-		dp[i] = make([]int, 4*OFFSET)
-		for j := range dp[i] {
-			dp[i][j] = -1
-		}
-	}
-	return dp
+func initDP() {
+    for i := range dp {
+        dp[i] = make([]int, 20000)
+        for j := range dp[i] {
+            dp[i][j] = -1
+        }
+    }
 }

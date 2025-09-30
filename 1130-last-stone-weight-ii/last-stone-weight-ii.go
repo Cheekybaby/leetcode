8f07d53c7b1d9587fc6ce12b7d062e0e7220255a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-09-30 22:10:39 +0530

    Time: 27 ms (7.14%) | Memory: 9.6 MB (7.14%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.go
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.go
@@ -1,34 +1,40 @@
 var dp = make([][]int, 32)
-const OFFSET = 3001
-func solve(nums []int, idx, sum int) int {
+func solve(nums []int, idx, sum, total int) int {
     if idx >= len(nums) {
-        if sum < 0 {
-            return 10000
-        }
-        return sum
+        return abs(total - 2 * sum)
     }
 
-    if dp[idx][OFFSET+sum] != -1 {
-        return dp[idx][OFFSET+sum]
+    if dp[idx][sum] != -1 {
+        return dp[idx][sum]
     }
 
-    add := solve(nums, idx+1, sum + nums[idx])
-    sub := solve(nums, idx+1, sum - nums[idx])
+    take := solve(nums, idx+1, sum + nums[idx], total)
+    not_take := solve(nums, idx+1, sum, total)
 
-    dp[idx][OFFSET+sum] = min(add, sub)
+    dp[idx][sum] = min(take, not_take)
     
-    return dp[idx][OFFSET+sum]
+    return dp[idx][sum]
 }
 func lastStoneWeightII(stones []int) int {
+    var total int
+    for _, val := range stones {
+        total += val
+    }
     initDP()
-    return solve(stones, 0, 0)
+    return solve(stones, 0, 0, total)
 }
 
 func initDP() {
     for i := range dp {
-        dp[i] = make([]int, 6002)
+        dp[i] = make([]int, 3001)
         for j := range dp[i] {
             dp[i][j] = -1
         }
     }
+}
+func abs(x int) int {
+    if x < 0 {
+        return -1 * x
+    }
+    return x
 }

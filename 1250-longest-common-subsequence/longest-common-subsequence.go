6e117e9af5c0739f661998983bc2f5f3a3f829c8
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-02 22:23:35 +0530

    Time: 27 ms (29.14%) | Memory: 21.5 MB (16.97%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.go
@@ -0,0 +1,32 @@
+func solve(s, t string, n, m int, dp[][]int) int {
+    if (n < 0) || (m < 0) {
+        return 0
+    }
+
+    if dp[n][m] != -1 {
+        return dp[n][m]
+    }
+
+    if s[n] == t[m] {
+        dp[n][m] = 1 + solve(s, t, n-1, m-1, dp)
+    } else {
+        dp[n][m] = max(solve(s, t, n-1, m, dp), solve(s, t, n, m-1, dp))
+    }
+    return dp[n][m]
+}
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+    n, m := len(text1)-1, len(text2)-1
+    dp := initDP(n, m)
+    return solve(text1, text2, n, m, dp)
+}
+func initDP(n, m int) (dp [][]int) {
+    dp = make([][]int, n+1)
+    for i := range dp {
+        dp[i] = make([]int, m+1)
+        for j := range dp[i] {
+            dp[i][j] = -1
+        }
+    }
+    return dp
+}

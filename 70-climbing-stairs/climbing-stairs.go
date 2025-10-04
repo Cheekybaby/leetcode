da0ed41892450228da91675a5e0f2deb7f71ec8b
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-04 12:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 3.9 MB (81.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.go
+++ 70-climbing-stairs/climbing-stairs.go
@@ -1,11 +1,27 @@
-func climbStairs(n int) int {
-    first, second := 1, 1
-
-    for i := 2; i <= n; i++ {
-        sum := first + second
-        first = second
-        second = sum
+func solve(n int, dp []int) int {
+    if n == 0 || n == 1 {
+        return 1
+    }
+    if n < 0 {
+        return 0
     }
 
-    return second
+    if dp[n] != -1 {
+        return dp[n]
+    }
+
+    dp[n] = solve(n-2, dp) + solve(n-1, dp)
+    return dp[n]
+}
+
+func climbStairs(n int) int {
+    dp := initDP(n)
+    return solve(n, dp)
+}
+func initDP(n int) (dp []int) {
+    dp = make([]int, n+1)
+    for i := range dp {
+        dp[i] = -1
+    }
+    return dp
 }

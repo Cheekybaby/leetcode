721c4afb5710d2364d9569c6524b389e377cc1fd
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-04 12:18:29 +0530

    Time: 1 ms (80.87%) | Memory: 3.8 MB (83.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.go
+++ 1013-fibonacci-number/fibonacci-number.go
@@ -1,24 +1,12 @@
-var dp = make([]int, 32);
-func solve(n int) int {
-    if n == 0 {
-        return 0
-    }
-    if n == 1 {
-        return 1
-    }
-    if dp[n] != -1 {
-        return dp[n]
-    }
-    dp[n] = solve(n-1) + solve(n-2)
-    return dp[n]
-}
 func fib(n int) int {
-    initDP()
-    return solve(n)
-}
-
-func initDP() {
-    for i := range dp {
-        dp[i] = -1
+    if n == 0 || n == 1 {
+        return n
     }
+    f, s := 0, 1
+    for i := 2; i <= n; i++ {
+        tmp := f + s
+        f = s
+        s = tmp
+    }
+    return s
 }

0c3414b7d6757ca4b49b28a43f742a4e4290834a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-04 12:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 5.5 MB (22.97%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.go
+++ 152-maximum-product-subarray/maximum-product-subarray.go
@@ -1,21 +1,17 @@
 func maxProduct(nums []int) int {
-    ans, product := math.MinInt64, 1
+    max_prod := math.MinInt
+    curr_max, curr_min := 1, 1
 
-    for i:=0; i<len(nums); i++ {
-        product *= nums[i]
-        ans = max(product, ans)
-        if product == 0 {
-            product = 1
+    for i := 0; i < len(nums); i++ {
+        if nums[i] < 0 {
+            curr_max, curr_min = curr_min, curr_max
         }
-    }
-    product = 1
-    for i:=len(nums)-1; i>=0; i-- {
-        product *= nums[i]
-        ans = max(product, ans)
-        if product == 0 {
-            product = 1
-        }
+
+        curr_max = max(curr_max*nums[i], nums[i])
+        curr_min = min(curr_min*nums[i], nums[i])
+
+        max_prod = max(max_prod, curr_max)
     }
 
-    return ans
+    return max_prod
 }

75e96241614cc9fa638f828b14641d578864d48a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-06 13:52:01 +0530

    Time: 22 ms (27.17%) | Memory: 15.1 MB (11.56%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.go
+++ 44-wildcard-matching/wildcard-matching.go
@@ -1,25 +1,56 @@
-func isMatch(s string, p string) bool {
-    i, j := 0, 0
-    idx, match := -1, 0
-    for i < len(s) {
-        if j < len(p) && (s[i] == p[j] || p[j] == '?'){
-            i++
-            j++
-        } else if j < len(p) && p[j] == '*' {
-            idx = j // Saw the asterisk
-            match = i // The idex where it matched
-            j++ // Ignored and moved on
-        } else if idx != -1 {
-            i = match + 1
-            j = idx + 1 
-            match++
-        } else {
-            return false
-        }
-    }
-    for j < len(p) && p[j] == '*' {
-        j++
+func solve(s, p string, n, m int, dp [][]int) int {
+    if n == len(s) && m == len(p) {
+        return 1
     }
 
-    return j == len(p)
+    if m == len(p) && n < len(s) {
+        return 0
+    }
+
+    if n == len(s) && m < len(p) {
+        for l := m; l < len(p); l++ {
+            if p[l] != '*' {
+                return 0
+            }
+        }
+        return 1
+    }
+
+    if dp[n][m] != -1 {
+        return dp[n][m]
+    }
+
+
+    if s[n] == p[m] {
+        dp[n][m] = solve(s, p, n+1, m+1, dp)
+    } else {
+        if p[m] == '?' {
+            dp[n][m] = solve(s, p, n+1, m+1, dp)
+        } else if p[m] == '*' {
+            val := solve(s, p, n+1, m, dp) + solve(s, p, n, m+1, dp)
+            if val > 0 {
+                dp[n][m] = 1
+            } else {
+                dp[n][m] = 0
+            }
+        } else { 
+            dp[n][m] = 0
+        }
+    }
+    return dp[n][m]
+}
+
+func isMatch(s string, p string) bool {
+    dp := initDP(len(s), len(p))
+    return (solve(s, p, 0, 0, dp) == 1)
+}
+func initDP(n, m int) (dp [][]int) {
+    dp = make([][]int, n+1)
+    for i := range dp {
+        dp[i] = make([]int, m+1)
+        for j := range dp[i] {
+            dp[i][j] = -1
+        }
+    }
+    return dp
 }

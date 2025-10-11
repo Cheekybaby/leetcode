4d83464a93866767d483e81e694ed289503c27b2
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-11 11:29:55 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (32.55%) - LeetSync

--- 198-house-robber/house-robber.go
+++ 198-house-robber/house-robber.go
@@ -1,12 +1,13 @@
 func rob(nums []int) int {
     n := len(nums)
-    dp := make([]int, n+1)
 
-    dp[n] = 0
-    dp[n-1] = nums[n-1]
+    f := 0
+    s := nums[n-1]
 
     for i := n-2; i >= 0; i-- {
-        dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        tmp := max(nums[i] + f, s)
+        f = s
+        s = tmp
     }
-    return dp[0]
+    return s
 }

60ea5a107c1183af743e22f79704958e61b77d76
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-12 14:57:46 +0530

    Time: 49 ms (49.56%) | Memory: 21.2 MB (53.10%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.go
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.go
@@ -1,32 +1,19 @@
-func solve(s string, i, j int, dp [][]int) int {
-    if i >= len(s) || j < 0 {
-        return 0
-    }
-
-    if dp[i][j] != -1 {
-        return dp[i][j]
-    }
-
-    if s[i] == s[j] {
-        dp[i][j] = 1 + solve(s, i+1, j-1, dp)
-    } else {
-        dp[i][j] = max(solve(s, i+1, j, dp), solve(s, i, j-1, dp))
-    }
-    
-    return dp[i][j]
-}
-
 func longestPalindromeSubseq(s string) int {
-    dp := initDP(len(s))
-    return solve(s, 0, len(s)-1, dp)
-}
-func initDP(n int) (dp [][]int) {
-    dp = make([][]int, n+1)
+    n := len(s)
+    dp := make([][]int, n+2)
     for i := range dp {
-        dp[i] = make([]int, n+1)
-        for j := range dp[i] {
-            dp[i][j] = -1
+        dp[i] = make([]int, n+2)
+    }
+
+    for i := n-1; i >= 0; i-- {
+        for j := 0; j < n; j++ {
+            if s[i] == s[j] {
+                dp[i+1][j+1] = 1 + dp[i+2][j]
+            } else {
+                dp[i+1][j+1] = max(dp[i+2][j+1], dp[i+1][j])
+            }
         }
     }
-    return dp
+
+    return dp[1][n]
 }

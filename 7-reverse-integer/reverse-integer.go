7650cbaaec81c7527868f622e7b5f12beaf40500
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-13 11:59:15 +0530

    Time: 3 ms (26.38%) | Memory: 4.1 MB (26.09%) - LeetSync

--- 7-reverse-integer/reverse-integer.go
+++ 7-reverse-integer/reverse-integer.go
@@ -1,49 +1,28 @@
 func reverse(x int) int {
-    if x <= 9 && x >= -9 {
-        return x
-    }
-    upperBound := "2147483647"
-    lowerBound := "2147483648"
     flag := false
-    ans := 0
     if x < 0 {
         flag = true
-        x = -1 * x
+        x *= -1
     }
 
-    xStr := strconv.Itoa(x)
-    rev := stringRev(xStr)
-
-    if flag == true {
-        if len(rev) >= len(lowerBound) && rev > upperBound {
-            return 0
-        } else {
-            val, _ := strconv.Atoi(rev)
-            ans = val
-        }
-    } else {
-        if len(rev) >= len(upperBound) && rev > upperBound {
-            return 0
-        } else {
-            val, _ := strconv.Atoi(rev)
-            ans = val
-        }
+    var rev []byte
+    for x > 0 {
+        digit := (byte(x % 10))
+        rev = append(rev, digit)
+        x /= 10
     }
+    fmt.Println(rev)
+
+    for i := range rev {
+        x = x * 10 + int(rev[i])
+    }
+
+    if x < -2147483648 || x > 2147483647 {
+        return 0
+    }
+
     if flag {
-        return -1 * ans
-    } else {
-        return ans
+        return x * -1
     }
-}
-
-func stringRev(xStr string) string {
-    rev := ""
-    for i:=len(xStr)-1; i>=0; i-- {
-        rev += string(xStr[i])
-    }
-    i := 0
-    for string(rev[i]) == "0" {
-        i++
-    }
-    return rev[i:len(rev)]
+    return x
 }

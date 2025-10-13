35a14caa9ed2ed0450db5598198bd30ad408556e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-13 22:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 3.9 MB (93.23%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.go
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.go
@@ -1,31 +1,26 @@
-func solve(index int, digits string, temp []byte, ans *[]string, keys []string) {
-    n := len(digits)
-
-    if index == n {
-        s := string(temp)
-        *ans = append(*ans, s)
+var keys = []string {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+func solve(digits string, idx int, comb []byte, poss_comb *[]string) {
+    if idx == len(digits) {
+        s := string(comb)
+        *poss_comb = append(*poss_comb, s)
         return 
     }
 
-    k := keys[(digits[index] - '0')]
-    for i := range k {
-        temp = append(temp, k[i])
-        solve(index+1, digits, temp, ans, keys)
-        temp = temp[:len(temp)-1]
+    key := keys[(digits[idx] - '0')]
+    for i := range key {
+        comb = append(comb, key[i])
+        solve(digits, idx+1, comb, poss_comb)
+        comb = comb[:len(comb)-1]
     }
+
 }
 
 func letterCombinations(digits string) []string {
     if len(digits) == 0 {
         return []string{}
     }
-
-    var temp []byte
     var ans []string
-    keys := []string{ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
-    
-
-    solve(0, digits, temp, &ans, keys)
-
+    var temp []byte
+    solve(digits, 0, temp, &ans)
     return ans
 }

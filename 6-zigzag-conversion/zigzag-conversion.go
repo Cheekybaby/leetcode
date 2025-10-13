4b06e024cdb30f4aadcdb07e95475b0d5eb4bcbf
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-13 11:43:33 +0530

    Time: 8 ms (44.05%) | Memory: 9.2 MB (39.62%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.go
+++ 6-zigzag-conversion/zigzag-conversion.go
@@ -1,20 +1,21 @@
 func convert(s string, numRows int) string {
-    if numRows == 1 || numRows > len(s) {
+    n := len(s)
+    if numRows == 1 || numRows >= n {
         return s
     }
-    var ans string
+
+    var zz string
     step := 2 * (numRows - 1)
 
-    for i:=0; i<numRows; i++ {
-        for j:=i; j<len(s); j+=step {
-            ans += string(s[j])
+    for i := 0; i < numRows; i++ {
+        for j := i; j < n; j+=step {
+            zz += string(s[j])
 
-            diag := j + step - 2 * i
-            if i > 0 && i < numRows-1 && diag < len(s) {
-                ans += string(s[diag])
+            diag := j + step - (2 * i)
+            if i > 0 && i < numRows-1 && diag < n {
+                zz += string(s[diag])
             }
         }
     }
-
-    return ans
+    return zz
 }

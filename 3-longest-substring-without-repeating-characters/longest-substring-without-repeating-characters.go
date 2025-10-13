de8d30941135478647fab5bbf6ea5396bf587aa5
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-13 11:15:44 +0530

    Time: 0 ms (100.00%) | Memory: 4.5 MB (86.65%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.go
@@ -1,34 +1,23 @@
 func lengthOfLongestSubstring(s string) int {
-	var freq [256]int
-	for k := range freq {
-		freq[k] = 0
-	}
-
+	n := len(s)
+	max_len := 0
 	i, j := 0, 0
-	maxLen := 0
+	var freq [256]int
+	for j < n {
+		char := (s[j])
+		val := freq[char]
 
-	for j = range s {
-		c := s[j]
-		val := freq[c]
-
-		if val > 0 {
-			// We will be resetting the window
-			for i < j && val != 0 {
-				freq[s[i]]--  // We remove the character on i from the map
-				val = freq[c] // See the value of current character in the map
-				i++           // Update the left pointer
-			}
+		for i < j && val > 0 {
+			ch := (s[i])
+			freq[ch]--
+			val = freq[char]
+			i++
 		}
-		maxLen = max(maxLen, j-i+1)
-		freq[c]++
+		
+        max_len = max(max_len, j-i+1)
+        freq[char]++
+		j++
 	}
 
-	return maxLen
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return max_len
 }

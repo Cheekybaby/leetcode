39700049cd76ff041f25242dac1f4fbd42a7653a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-15 10:40:22 +0530

    Time: 7 ms (50.70%) | Memory: 11.3 MB (11.54%) - LeetSync

--- 38-count-and-say/count-and-say.go
+++ 38-count-and-say/count-and-say.go
@@ -1,14 +1,5 @@
-func countAndSay(n int) string {
-    if n == 1 {
-        return "1"
-    }
-    if n == 2 {
-        return "11"
-    }
-
+func helper(s string) string {
     var res string
-    s := countAndSay(n-1)
-
     for i := 0; i < len(s); i++ {
         char := s[i]
         count := 0
@@ -22,6 +13,16 @@ func countAndSay(n int) string {
     }
 
     return res
+}
+func countAndSay(n int) string {
+    dp := make([]string, n)
+    
+    dp[0] = "1"
+
+    for i := 1; i < n; i++ {
+        dp[i] = helper(dp[i-1])
+    }
+    return dp[n-1]
 }
 func toString(n int) string {
     return strconv.Itoa(n)

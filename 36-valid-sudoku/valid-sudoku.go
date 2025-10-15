1f7211a596eef57a2b433198f604de59e984c1f8
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-15 09:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 4.7 MB (79.67%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.go
+++ 36-valid-sudoku/valid-sudoku.go
@@ -1,55 +1,53 @@
 func isValidSudoku(board [][]byte) bool {
-	// Check all the rows
+	// Rows
 	for i := 0; i < 9; i++ {
-		set := make(map[byte]int)
+		set := make(map[int]bool)
 		for j := 0; j < 9; j++ {
 			char := board[i][j]
-
 			if char == '.' {
 				continue
 			}
-
-			if _, ok := set[char]; ok {
+			digit := int(char - '0')
+			if _, ok := set[digit]; ok {
 				return false
 			}
-			set[char]++
+			set[digit] = true
 		}
 	}
-	// Check all the columns
+	// Columns
 	for i := 0; i < 9; i++ {
-		set := make(map[byte]int)
+		set := make(map[int]bool)
 		for j := 0; j < 9; j++ {
 			char := board[j][i]
-
 			if char == '.' {
 				continue
 			}
-
-			if _, ok := set[char]; ok {
+			digit := int(char - '0')
+			if _, ok := set[digit]; ok {
 				return false
 			}
-			set[char]++
+			set[digit] = true
 		}
 	}
-	// Check all 9 3x3 submatrix
-	for r := 0; r < 9; r += 3 {
-		for c := 0; c < 9; c += 3 {
-			set := make(map[byte]int)
-			for i := r; i < r+3; i++ {
-				for j := c; j < c+3; j++ {
+	// 3x3 grids
+	for row := 0; row < 9; row += 3 {
+		for col := 0; col < 9; col += 3 {
+			set := make(map[int]bool)
+			for i := row; i < row+3; i++ {
+				for j := col; j < col+3; j++ {
 					char := board[i][j]
-
 					if char == '.' {
 						continue
 					}
-
-					if _, ok := set[char]; ok {
+					digit := int(char - '0')
+					if _, ok := set[digit]; ok {
 						return false
 					}
-					set[char]++
+					set[digit] = true
 				}
 			}
 		}
 	}
+
 	return true
 }

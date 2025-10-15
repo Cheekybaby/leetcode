dc2c9d903facba2d2c26eedf737b54eea3f0939c
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-15 09:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 4.5 MB (33.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -1,24 +1,23 @@
 func search(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-
-	for left <= right {
-		mid := left + (right-left)/2
-
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] >= nums[left] {
-			if nums[left] <= target && target <= nums[mid] {
-				right = mid - 1
-			} else {
-				left = mid + 1
-			}
-		} else {
-			if nums[mid] <= target && target <= nums[right] {
-				left = mid + 1
-			} else {
-				right = mid - 1
-			}
-		}
-	}
-	return -1
+    l, r := 0, len(nums)-1
+    for l <= r {
+        mid := l + (r - l)/2
+        
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] >= nums[l] {
+            if target >= nums[l] && target < nums[mid] {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        } else {
+            if target > nums[mid] && target <= nums[r] {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+    }
+    return -1
 }

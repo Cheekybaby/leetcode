ecd075852d7989135fca0eb5b9159a23f3c4bdcf
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-16 23:10:32 +0530

    Time: 0 ms (100.00%) | Memory: 4.7 MB (29.90%) - LeetSync

--- 46-permutations/permutations.go
+++ 46-permutations/permutations.go
@@ -1,62 +1,28 @@
-func factorial(n int) int {
-	fact := 1
-	for i := 1; i <= n; i++ {
-		fact *= i
-	}
+func solve(nums []int, idx int, temp []int, ans *[][]int, set map[int]int) {
+    if idx == len(nums) {
+        tmp := make([]int, len(temp))
+        copy(tmp, temp)
+        *ans = append(*ans, tmp)
 
-	return fact
-}
+        return
+    }
 
-func nextPermutation(nums []int) []int {
-	n := len(nums)
-	idx := n - 2
-	for idx >= 0 {
-		if nums[idx] < nums[idx+1] {
-			break
-		}
-		idx--
-	}
-
-	if idx != -1 {
-		mini_idx, mini := idx, math.MaxInt
-		for i := idx + 1; i < n; i++ {
-			if nums[i] > nums[idx] {
-				if nums[i] <= mini {
-					mini_idx = i
-					mini = nums[i]
-				}
-			}
-		}
-
-		nums[idx], nums[mini_idx] = nums[mini_idx], nums[idx]
-	}
-
-	reverse(nums, idx+1, n-1)
-
-	return nums
+    for i := range nums {
+        if _, ok := set[nums[i]]; !ok {
+            temp = append(temp, nums[i])
+            set[nums[i]] = 1
+            solve(nums, idx+1, temp, ans, set)
+            delete(set, nums[i])
+            temp = temp[:len(temp)-1]
+        }
+    }
 }
 
 func permute(nums []int) [][]int {
-	sort.Ints(nums)
-	n := factorial(len(nums))
+    var temp []int
+    var ans [][]int
+    set := make(map[int]int)
+    solve(nums, 0, temp, &ans, set)
 
-	permutations := make([][]int, n)
-
-	permutations[0] = append([]int(nil), nums...)
-	for i := 1; i < n; i++ {
-		perm := append([]int(nil), permutations[i-1]...)
-		nxt := nextPermutation(perm)
-
-		permutations[i] = append([]int(nil), nxt...)
-	}
-
-	return permutations
-}
-
-func reverse(nums []int, i, j int) {
-	for i < j {
-		nums[i], nums[j] = nums[j], nums[i]
-		i++
-		j--
-	}
+    return ans
 }

feaa15695eb5d8c0b48106fe78822c866f443773
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-16 22:33:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.37%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.go
+++ 45-jump-game-ii/jump-game-ii.go
@@ -1,42 +1,18 @@
-func solve(nums []int, idx int, dp []int) int {
-    if idx >= len(nums) {
-        return math.MaxInt
-    }
-    if idx == len(nums)-1 {
-        return 1
-    }
+func jump(nums []int) int {
+    n := len(nums)
+    jumps := 0
+    farthest := 0
+    end := 0
+    for i := 0; i < n-1; i++ {
+        farthest = max(farthest, i + nums[i])
+        if i == end {
+            jumps++
+            end = farthest
 
-    j := nums[idx]
-
-    if j == 0 {
-        return math.MaxInt
-    }
-    
-    if dp[idx] != -1 {
-        return dp[idx]
-    }
-
-    ans := len(nums)
-    for i := idx+1; i <= idx + j; i++ {
-        val := solve(nums, i, dp)
-        if val != math.MaxInt {
-            ans = min(1 + val, ans)
+            if end >= n-1 {
+                break
+            }
         }
     }
-
-    dp[idx] = ans
-
-    return dp[idx]
-}
-
-func jump(nums []int) int {
-    dp := initDP(len(nums))
-    return solve(nums, 0, dp) - 1
-}
-func initDP(n int) (dp []int) {
-    dp = make([]int, n+1)
-    for i := range dp {
-        dp[i] = -1
-    }
-    return dp
+    return jumps
 }

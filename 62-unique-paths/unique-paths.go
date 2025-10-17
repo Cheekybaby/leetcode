7dcb91a2a9056012ac8cd1744cca76c3ceb1d0bb
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-17 15:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (34.81%) - LeetSync

--- 62-unique-paths/unique-paths.go
+++ 62-unique-paths/unique-paths.go
@@ -1,35 +1,21 @@
-func solve(m, n int, dp [][]int) int {
-    if m == 1 && n == 1 {
-        return 1
-    }
-    if m < 1 || n < 1 {
-        return 0
-    }
-
-    if dp[m][n] != -1 {
-        return dp[m][n]
-    }
-
-    left := solve(m, n-1, dp)
-    up := solve(m-1, n, dp)
-
-    dp[m][n] = (left + up)
-
-    return dp[m][n]
-}
-
 func uniquePaths(m int, n int) int {
-    dp := initDP(m, n)
-    return solve(m, n, dp)
-}
-func initDP(m, n int) (dp [][]int) {
-    dp = make([][]int, m+1)
+    dp := make([][]int, m)
     for i := range dp {
-        dp[i] = make([]int, n+1)
-        for j := range dp[i] {
-            dp[i][j] = -1
+        dp[i] = make([]int, n)
+    }
+
+    for i := 0; i < n; i++ {
+        dp[0][i] = 1
+    }
+    for i := 0; i < m; i++ {
+        dp[i][0] = 1
+    }
+
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
         }
     }
 
-    return dp
+    return dp[m-1][n-1]
 }

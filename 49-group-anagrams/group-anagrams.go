8954ae1512fc8aad9101f1a9ea05583bbbae98aa
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-17 10:03:37 +0530

    Time: 12 ms (49.60%) | Memory: 9.5 MB (65.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,28 +1,19 @@
 func groupAnagrams(strs []string) [][]string {
-    groupMap := make(map[string][]string)
+    set := make(map[string][]string)
     for i := range strs {
         str := strs[i]
-        temp := stringSort(str)
-        groupMap[temp] = append(groupMap[temp], str)
+        r := []rune(str)
+        sort.Slice(r, func(i, j int) bool {
+            return r[i] < r[j]
+        })
+        str = string(r)
+        set[str] = append(set[str], strs[i])
     }
 
     var groups [][]string
-    for i := range groupMap {
-        val, ok := groupMap[i]
-        if ok {
-            groups = append(groups, val)
-        }
+    for _, val := range set {
+        groups = append(groups, val)
     }
+
     return groups
-}
-
-func stringSort(s string) (t string) {
-    runes := []rune(s)
-
-    sort.Slice(runes, func(i, j int) bool {
-        return runes[i] < runes[j]
-    })
-
-    t = string(runes)
-    return t
 }

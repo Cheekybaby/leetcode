61fa7b0c2625bc70d9a155e10745a3999bab2297
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-17 09:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (59.94%) - LeetSync

--- 48-rotate-image/rotate-image.go
+++ 48-rotate-image/rotate-image.go
@@ -1,20 +1,22 @@
 func rotate(matrix [][]int)  {
-    for i := range matrix {
+    n := len(matrix)
+    for i := 0; i < n; i++ {
         for j := 0; j <= i; j++ {
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         }
     }
 
-    for i := range matrix {
-        reverse(matrix[i])
+    for i := 0; i < n; i++ {
+        row := matrix[i]
+        reverse(row)
+        matrix[i] = row
     }
 }
-
 func reverse(row []int) {
-    l, r := 0, len(row)-1
-    for l < r {
-        row[l], row[r] = row[r], row[l]
-        l++
-        r--
+    i, j := 0, len(row)-1
+    for i < j {
+        row[i], row[j] = row[j], row[i]
+        i++
+        j--
     }
 }

611a4b2ce5afde89520555474a4b9d15cac8f483
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-17 10:13:00 +0530

    Time: 2 ms (18.75%) | Memory: 11.1 MB (22.27%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,37 +1,14 @@
 func maxSubArray(nums []int) int {
-    return helper(nums, 0, len(nums)-1)
-}
-
-func helper(nums []int, left, right int) int {
-    if left == right {
-        return nums[left]
-    }
-
-    mid := (left + right)/2
-
-    left_sum := helper(nums, left, mid)
-    right_sum := helper(nums, mid+1, right)
-    cross_sum := crossSum(nums, left, mid, right)
-
-    return max(left_sum, max(right_sum, cross_sum))
-}
-
-func crossSum(nums []int, left, mid, right int) int {
-    left_sum := math.MinInt
     sum := 0
+    max_sum := math.MinInt
+    for i := range nums {
+        sum = max(sum + nums[i], nums[i])
+        max_sum = max(sum, max_sum)
 
-    for i := mid; i >= left; i-- {
-        sum += nums[i]
-        left_sum = max(left_sum, sum)
+        if sum < 0 {
+            sum = 0
+        }
     }
 
-    right_sum := math.MinInt
-    sum = 0
-    
-    for i := mid + 1; i <= right; i++ {
-        sum += nums[i]
-        right_sum = max(right_sum, sum)
-    }
-
-    return left_sum + right_sum
+    return max_sum
 }

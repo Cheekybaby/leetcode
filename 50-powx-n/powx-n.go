49fd830744d6df8aea46a1a69060611ea5f0115c
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-17 10:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (78.15%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,21 +1,27 @@
-func myPow(x float64, n int) float64 {
-    return findPow(x, n)
-}
-
-func findPow(x float64, n int) float64 {
+func pow(x float64, n int) float64 {
     if n == 0 {
         return 1
     }
 
-    if n < 0 {
-        return 1.0/findPow(x, -n)
-    }
-
-    if (n % 2) == 1 {
-        res := findPow(x, n/2)
-        return x * res *  res
+    if n % 2 == 1 {
+        val := pow(x, n/2)
+        return x * val * val
     } else {
-        res := findPow(x, n/2)
-        return res * res
+        val := pow(x, n/2)
+        return val * val
     }
+}
+
+func myPow(x float64, n int) float64 {
+    neg := false
+    if n < 0 {
+        n *= -1
+        neg = true
+    }
+    ans := pow(x, n)
+
+    if neg {
+        return 1/ans
+    }
+    return ans
 }

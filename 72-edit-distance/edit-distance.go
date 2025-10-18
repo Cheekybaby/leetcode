e85f99244e57af9b84c5a841aea4c22cf5f9659b
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-18 11:14:08 +0530

    Time: 2 ms (54.98%) | Memory: 7.8 MB (22.99%) - LeetSync

--- 72-edit-distance/edit-distance.go
+++ 72-edit-distance/edit-distance.go
@@ -1,34 +1,35 @@
-func solve(s, t string, n, m int, dp [][]int) int {
-	if n == len(s) {
-		return len(t) - m
+func solve(s, t string, i, j int, dp [][]int) int {
+	if i == len(s) {
+		return len(t) - j
 	}
-	if m == len(t) {
-		return len(s) - n
-	}
-
-	if dp[n][m] != -1 {
-		return dp[n][m]
+	if j == len(t) {
+		return len(s) - i
 	}
 
-	if s[n] == t[m] {
-		dp[n][m] = solve(s, t, n+1, m+1, dp)
+    if dp[i][j] != -1 {
+        return dp[i][j]
+    }
+
+	if s[i] == t[j] {
+		dp[i][j] = solve(s, t, i+1, j+1, dp)
 	} else {
-		dp[n][m] = 1 + min(solve(s, t, n+1, m+1, dp), min(solve(s, t, n, m+1, dp), solve(s, t, n+1, m, dp)))
+		dp[i][j] = 1 + min(solve(s, t, i+1, j+1, dp), min(solve(s, t, i+1, j, dp), solve(s, t, i, j+1, dp)))
 	}
-	return dp[n][m]
+
+    return dp[i][j]
 }
 
 func minDistance(word1 string, word2 string) int {
-	dp := initDP(len(word1), len(word2))
+    dp := initDP(len(word1), len(word2))
 	return solve(word1, word2, 0, 0, dp)
 }
 func initDP(n, m int) (dp [][]int) {
-	dp = make([][]int, n+1)
-	for i := range dp {
-		dp[i] = make([]int, m + 1)
-		for j := range dp[i] {
-			dp[i][j] = -1
-		}
-	}
-	return dp
+    dp = make([][]int, n+1)
+    for i := range dp {
+        dp[i] = make([]int, m+1)
+        for j := range dp[i] {
+            dp[i][j] = -1
+        }
+    }
+    return dp
 }

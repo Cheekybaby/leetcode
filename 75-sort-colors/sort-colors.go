5f42031d6a4dab5010208e0a124af627977cd52f
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-19 00:10:02 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (53.57%) - LeetSync

---
+++ 75-sort-colors/sort-colors.go
@@ -0,0 +1,17 @@
+func sortColors(nums []int)  {
+    n := len(nums)
+    l, m, r := 0, 0, n-1
+
+    for m <= r {
+        if nums[m] == 0 {
+            nums[l], nums[m] = nums[m], nums[l]
+            l++
+            m++
+        } else if nums[m] == 1 {
+            m++
+        } else {
+            nums[m], nums[r] = nums[r], nums[m]
+            r--
+        }
+    }
+}

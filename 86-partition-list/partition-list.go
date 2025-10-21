811ee1251a2d4da227e4f945233de6498f2223af
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-21 08:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 4.3 MB (53.21%) - LeetSync

---
+++ 86-partition-list/partition-list.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func partition(head *ListNode, x int) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+	var nodes []*ListNode
+
+	dummy := head
+	for dummy != nil {
+		if dummy.Val < x {
+			nodes = append(nodes, dummy)
+		}
+		dummy = dummy.Next
+	}
+	dummy = head
+	for dummy != nil {
+        if dummy.Val >= x {
+		    nodes = append(nodes, dummy)
+        }
+		dummy = dummy.Next
+	}
+
+	newHead := nodes[0]
+	dummy = newHead
+	for i := 0; i < len(nodes); i++ {
+		if i == len(nodes)-1 {
+			dummy.Next = nil
+			continue
+		}
+		dummy.Next = nodes[i+1]
+		dummy = dummy.Next
+	}
+
+	return newHead
+}

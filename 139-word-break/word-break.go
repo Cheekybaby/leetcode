39dbad9761f9f1a0049f55eda0582c00d32fe13a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-10-28 14:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 4.2 MB (32.06%) - LeetSync

--- 139-word-break/word-break.go
+++ 139-word-break/word-break.go
@@ -1,34 +1,39 @@
-func solve(s string, idx int, dict []string, dp []int) bool {
-	if idx < 0 {
-		return true
-	}
+func solve(s string, wordDict []string, idx int, dp []int) bool {
+    if idx >= len(s) {
+        return true
+    }
 
     if dp[idx] != -1 {
         return dp[idx] == 1
     }
 
-	dp[idx] = 0
-	for _, str := range dict {
-		length := len(str)
-		if idx-length+1 < 0 {
-			continue
-		}
-		if (s[idx-length+1:idx+1] == str) && solve(s, idx-length, dict, dp) {
-			dp[idx] = 1
-		}
-	}
+    dp[idx] = 0
+
+    for _, str := range wordDict {
+        length := len(str)
+
+        if idx+length > len(s) {
+            continue
+        }
+
+        if (s[idx:idx+length] == str) && solve(s, wordDict, idx+length, dp) {
+            dp[idx] = 1
+        }
+    }
 
     return dp[idx] == 1
 }
 
 func wordBreak(s string, wordDict []string) bool {
     dp := initDP(len(s))
-	return solve(s, len(s)-1, wordDict, dp)
+    return solve(s, wordDict, 0, dp)
 }
+
 func initDP(n int) (dp []int) {
-    dp = make([]int, n)
+    dp = make([]int, n+1)
     for i := range dp {
         dp[i] = -1
     }
+
     return dp
 }

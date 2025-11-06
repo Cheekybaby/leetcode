51527eb7f708bb34bcd1806a5e583bc452293759
Shashank Ranjan <shashankranjan07@gmail.com>
2025-11-06 21:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (0.00%) - LeetSync

--- 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.go
+++ 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.go
@@ -1,16 +1,27 @@
 func maximumPoints(enemy []int, energy int) int64 {
-    sort.Ints(enemy)
-    if enemy[0] > energy {
+    min_energy := enemy[0]
+    min_idx := 0
+    for i, e := range enemy {
+        if e < min_energy {
+            min_energy = e
+            min_idx = i
+        }
+    }
+
+    if min_energy > energy {
         return 0
     }
 
     points := int64(0)
     total := int64(energy)
-    for i := 1; i < len(enemy); i++ {
+    for i := 0; i < len(enemy); i++ {
+        if i == min_idx {
+            continue
+        }
         total += int64(enemy[i])
     }
 
-    points = total / int64(enemy[0])
+    points = total / int64(min_energy)
 
     return points
 }

f1fb6eff3107981780eb901ffda93a9c992843c1
Shashank Ranjan <shashankranjan07@gmail.com>
2025-11-08 22:45:37 +0530

    Time: 2 ms (37.50%) | Memory: 4.6 MB (100.00%) - LeetSync

--- 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.go
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.go
@@ -1,64 +1,29 @@
 func numSpecialEquivGroups(words []string) int {
-    visited := make([]bool, len(words))
-    groups := 0
+    groups := make(map[string]bool)
 
-    for i := 0; i < len(words); i++ {
-        if visited[i] == true {
-            continue
-        }
-
-        var odd, even []byte
-
-        for idx := range words[i] {
-            if idx % 2 == 0 {
-                even = append(even, words[i][idx])
-            } else {
-                odd = append(odd, words[i][idx])
-            }
-        }
-
-        sort.Slice(even, func(l, r int) bool {
-            return even[l] < even[r]
-        })
-        sort.Slice(odd, func(l, r int) bool {
-            return odd[l] < odd[r]
-        })
-
-        for j := i + 1; j < len(words); j++ {
-            if visited[j] == true {
-                continue
-            }
-
-            var curr_odd, curr_even []byte
-            for idx := range words[j] {
-                if idx % 2 == 0 {
-                    curr_even = append(curr_even, words[j][idx])
-                } else {
-                    curr_odd = append(curr_odd, words[j][idx])
-                }
-            }
-
-            sort.Slice(curr_even, func(l, r int) bool {
-                return curr_even[l] < curr_even[r]
-            })
-            sort.Slice(curr_odd, func(l, r int) bool {
-                return curr_odd[l] < curr_odd[r]
-            })
-
-            if compare(curr_odd, odd) && compare(curr_even, even) {
-                visited[j] = true
-            }
-        }
-
-        groups++
+    for i := range words {
+        word := []byte(words[i])
+        sortstr(word)
+        groups[string(word)] = true
     }
-    return groups
+
+    return len(groups)
 }
-func compare(arr1, arr2 []byte) bool {
-    for i := 0; i < len(arr1); i++ {
-        if arr1[i] != arr2[i] {
-            return false
+
+func sortstr(word []byte) {
+    for i := 0; i < len(word); i+=2 {
+        for j := i+2; j < len(word); j+=2 {
+            if word[i] > word[j] {
+                word[i], word[j] = word[j], word[i]
+            }
         }
     }
-    return true
+
+    for i := 1; i < len(word); i+=2 {
+        for j := i+2; j < len(word); j+=2 {
+            if word[i] > word[j] {
+                word[i], word[j] = word[j], word[i]
+            }
+        }
+    }
 }

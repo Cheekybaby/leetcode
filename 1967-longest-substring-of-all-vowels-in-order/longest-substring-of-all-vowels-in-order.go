2a703ba153ecc96ae64592150bb8cd2105501657
Shashank Ranjan <shashankranjan07@gmail.com>
2025-11-14 10:18:29 +0530

    Time: 56 ms (23.08%) | Memory: 9.2 MB (76.92%) - LeetSync

--- 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.go
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.go
@@ -1,20 +1,51 @@
 func longestBeautifulSubstring(word string) int {
-    cnt, length := 1, 1
     max_len := 0
+    for i := 0; i < len(word); i++ {
+        if word[i] == 'a' {
+            set := make(map[byte]int)
+            set[word[i]]++
+            j := i + 1
+            for j < len(word) {
+                if word[j] == word[j-1] {
+                    set[word[j]]++
+                } else if word[j] == 'e' {
+                    if word[j-1] == 'a' {
+                        set[word[j]]++
+                    } else {
+                        break
+                    }
+                } else if word[j] == 'i' {
+                    if word[j-1] == 'e' {
+                        set[word[j]]++
+                    } else {
+                        break
+                    }
+                } else if word[j] == 'o' {
+                    if word[j-1] == 'i' {
+                        set[word[j]]++
+                    } else {
+                        break
+                    }
+                } else if word[j] == 'u' {
+                    if word[j-1] == 'o' {
+                        set[word[j]]++
+                    } else {
+                        break
+                    }
+                } else {
+                    break
+                }
+                j+=1
+            }
+            i = j-1
+            if len(set) == 5 {
+                length := 0
+                for _, val := range set {
+                    length += val
+                }
 
-    for i := 0; i < len(word)-1; i++ {
-        if word[i+1] == word[i] {
-            length++
-        } else if word[i+1] > word[i] {
-            cnt++
-            length++
-        } else {
-            cnt = 1
-            length = 1
-        }
-
-        if cnt == 5 {
-            max_len = max(max_len, length)
+                max_len = max(max_len, length)
+            }
         }
     }
 

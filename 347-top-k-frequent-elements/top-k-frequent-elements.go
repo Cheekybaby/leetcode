df7a467564af2ce34b1c5d35de713eed2c366a39
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-02 16:28:16 +0530

    Time: 1 ms (69.63%) | Memory: 8.2 MB (28.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,23 +1,24 @@
 func topKFrequent(nums []int, k int) []int {
-    bucket := make([][]int, len(nums)+1)
-    freq := make(map[int]int)
-    var ans []int
+    buckets := make([][]int, len(nums))
+    freq := map[int]int{}
+
     for _, num := range nums {
         freq[num]++
     }
 
-    for k, v := range freq {
-        bucket[v] = append(bucket[v], k)
+    for key, val := range freq {
+        buckets[val-1] = append(buckets[val-1], key)
     }
 
-
-    for i := len(bucket)-1; i>=0 && k > 0 ; i-- {
-        buck := bucket[i]
-        for j:=0; j<len(buck); j++ {
-            ans = append(ans, buck[j])
-            k--
+    var ans []int
+    for i := len(buckets)-1; i >= 0 && k > 0; i-- {
+        elements := buckets[i]
+        if len(elements) > 0 {
+            for _, ele := range elements {
+                ans = append(ans, ele)
+                k--
+            }
         }
     }
-
     return ans
 }

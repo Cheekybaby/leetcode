9a26db8a4b402e537bc7bcd5d8b7b2156b3d214a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-03 10:08:20 +0530

    Time: 0 ms (100.00%) | Memory: 4.6 MB (98.52%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.go
+++ 36-valid-sudoku/valid-sudoku.go
@@ -1,53 +1,46 @@
 func isValidSudoku(board [][]byte) bool {
-	// Rows
-	for i := 0; i < 9; i++ {
-		set := make(map[int]bool)
-		for j := 0; j < 9; j++ {
-			char := board[i][j]
-			if char == '.' {
-				continue
-			}
-			digit := int(char - '0')
-			if _, ok := set[digit]; ok {
-				return false
-			}
-			set[digit] = true
-		}
-	}
-	// Columns
-	for i := 0; i < 9; i++ {
-		set := make(map[int]bool)
-		for j := 0; j < 9; j++ {
-			char := board[j][i]
-			if char == '.' {
-				continue
-			}
-			digit := int(char - '0')
-			if _, ok := set[digit]; ok {
-				return false
-			}
-			set[digit] = true
-		}
-	}
-	// 3x3 grids
-	for row := 0; row < 9; row += 3 {
-		for col := 0; col < 9; col += 3 {
-			set := make(map[int]bool)
-			for i := row; i < row+3; i++ {
-				for j := col; j < col+3; j++ {
-					char := board[i][j]
-					if char == '.' {
-						continue
-					}
-					digit := int(char - '0')
-					if _, ok := set[digit]; ok {
-						return false
-					}
-					set[digit] = true
-				}
-			}
-		}
-	}
-
-	return true
+    // check all the rows
+    for i := 0; i < 9; i++ {
+        seen := map[byte]bool{}
+        for j := 0; j < 9; j++ {
+            if board[i][j] == '.' {
+                continue
+            }
+            if _, ok := seen[board[i][j]]; ok {
+                return false
+            }
+            seen[board[i][j]] = true
+        }
+    }
+    // check all the columns
+    for i := 0; i < 9; i++ {
+        seen := map[byte]bool{}
+        for j := 0; j < 9; j++ {
+            if board[j][i] == '.' {
+                continue
+            }
+            if _, ok := seen[board[j][i]]; ok {
+                return false
+            }
+            seen[board[j][i]] = true
+        }
+    }
+    // check all 3x3 submatrices
+    for row := 0; row < 9; row += 3 {
+        for col := 0; col < 9; col += 3 {
+            seen := map[byte]bool{}
+            for i := row; i < row + 3; i++ {
+                for j := col; j < col + 3; j++ {
+                    if board[i][j] == '.' {
+                        continue
+                    }
+                    if _, ok := seen[board[i][j]]; ok {
+                        return false
+                    }
+                    seen[board[i][j]] = true
+                }
+            }
+        }
+    }
+    return true
 }

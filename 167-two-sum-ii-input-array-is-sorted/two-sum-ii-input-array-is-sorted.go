677c2fa009ee95427e56fd5b073bf61ff4d10bad
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-03 19:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (41.16%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,16 +1,29 @@
-func twoSum(nums []int, target int) []int {
-    i, j := 0, len(nums)-1
-    for i < j {
-        sum := nums[i] + nums[j]
+func twoSum(numbers []int, target int) []int {
+    for i := range numbers {
+        toFind := target - numbers[i]
 
-        if sum == target {
-            return []int{i+1, j+1}
-        } else if sum > target {
-            j--
-        } else {
-            i++
+        idx := binarySearch(numbers, i+1, toFind)
+
+        if idx != -1 {
+            return []int{i+1, idx+1}
         }
     }
 
-    return []int{j+1, i+1}
+    return []int{-1,-1}
+}
+func binarySearch(nums []int, st, target int) int {
+    end := len(nums)-1
+    for st <= end {
+        mid := st + (end - st)/2
+
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] > target {
+            end = mid - 1
+        } else {
+            st = mid + 1
+        }
+    }
+
+    return -1
 }

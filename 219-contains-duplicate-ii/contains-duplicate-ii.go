3c974c813a731d11f7e001bcd1f91bc28e2103e5
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-04 09:14:57 +0530

    Time: 27 ms (87.17%) | Memory: 14.1 MB (43.69%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.go
+++ 219-contains-duplicate-ii/contains-duplicate-ii.go
@@ -2,20 +2,29 @@ func containsNearbyDuplicate(nums []int, k int) bool {
     if k == 0 {
         return false
     }
-    set := make(map[int]bool)
     j := 0
-    for i:=0; i<len(nums); i++ {
-        length := i - j
-        if length <= k {
-            if _, ok := set[nums[i]]; ok {
-                return true
-            }
-            if length == k {
-                delete(set, nums[j])
-                j+=1
+    seen := map[int]int{}
+    for i := 0; i < len(nums); i++ {
+        curr := nums[i]
+
+        win_len := i - j
+
+        if win_len > k {
+            // now shrink the window 
+            for win_len > k {
+                temp := nums[j]
+                delete(seen, temp)
+                j++
+                win_len = i - j
             }
         }
-        set[nums[i]] = true
+
+        // Now check if there are duplicates or not
+        if _, ok := seen[curr]; ok {
+            return true
+        }
+        seen[nums[i]]++
     }
+
     return false
 }

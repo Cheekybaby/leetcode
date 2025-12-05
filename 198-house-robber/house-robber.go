18f7c34c39893db57dd7c922c9b988b45c417ad7
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-05 21:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (76.00%) - LeetSync

--- 198-house-robber/house-robber.go
+++ 198-house-robber/house-robber.go
@@ -1,27 +1,12 @@
-func solve(nums []int, idx int, dp []int) int {
-    if idx >= len(nums) {
-        return 0
-    }
-
-    if dp[idx] != -1 {
-        return dp[idx]
-    }
-
-    rob := nums[idx] +  solve(nums, idx+2, dp)
-    not_rob := solve(nums, idx+1, dp)
-
-    dp[idx] = max(rob, not_rob)
-
-    return dp[idx]
-}
-
 func rob(nums []int) int {
-    dp := make([]int, len(nums))
-    for i := range dp {
-        dp[i] = -1
+    dp := make([]int, len(nums)+1)
+    dp[1] = nums[0]
+
+    for i := 1; i < len(nums); i++ {
+        rob := nums[i] + dp[i-1]
+        not_rob := dp[i]
+        dp[i+1] = max(rob, not_rob)
     }
 
-    max_rob := solve(nums, 0, dp)
-
-    return max_rob
+    return dp[len(nums)]
 }

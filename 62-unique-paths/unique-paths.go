7dee397f938e1f227bdea3e255be7c8a1d439598
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-06 00:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (38.24%) - LeetSync

--- 62-unique-paths/unique-paths.go
+++ 62-unique-paths/unique-paths.go
@@ -1,31 +1,18 @@
-func solve(i, j int, dp [][]int) int {
-    if i == 0 && j == 0 {
-        return 1
-    }
-    if i < 0 || j < 0 {
-        return 0
-    }
-
-    if dp[i][j] != -1 {
-        return dp[i][j]
-    }
-    
-    left := solve(i, j-1, dp)
-    up := solve(i-1, j, dp)
-
-    dp[i][j] = left + up
-
-    return dp[i][j]
-}
-
 func uniquePaths(m int, n int) int {
     dp := make([][]int, m)
     for i := range dp {
         dp[i] = make([]int, n)
-        for j := range dp[i] {
-            dp[i][j] = -1
+    }
+    
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if i == 0 || j == 0 {
+                dp[i][j] = 1
+                continue
+            }
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
         }
     }
-    total_paths := solve(m-1, n-1, dp)
-    return total_paths
+
+    return dp[m-1][n-1]
 }

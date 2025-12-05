cc4fe2f2d0d418509cffa806d44a783d53e7cced
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-06 00:35:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (23.33%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.go
+++ 695-max-area-of-island/max-area-of-island.go
@@ -12,13 +12,13 @@ func maxAreaOfIsland(grid [][]int) int {
             if grid[i][j] == 1 && visited[i][j] == 0 {
                 cnt := 0 // to keep track of size of the island
                 
-                var stack [][2]int
-                stack = append(stack, [2]int{i, j})
+                var q [][2]int
+                q = append(q, [2]int{i, j})
                 visited[i][j] = 1
                 // bfs
-                for len(stack) > 0 {
-                    node := stack[len(stack)-1]
-                    stack = stack[:len(stack)-1]
+                for len(q) > 0 {
+                    node := q[0]
+                    q = q[1:]
 
                     r, c := node[0], node[1]
 
@@ -26,22 +26,22 @@ func maxAreaOfIsland(grid [][]int) int {
                     
                     // up
                     if r > 0 && visited[r-1][c] == 0 && grid[r-1][c] == 1 {
-                        stack = append(stack, [2]int{r-1, c})
+                        q = append(q, [2]int{r-1, c})
                         visited[r-1][c] = 1
                     }
                     // right
                     if c < n-1 && visited[r][c+1] == 0 && grid[r][c+1] == 1 {
-                        stack = append(stack, [2]int{r, c+1})
+                        q = append(q, [2]int{r, c+1})
                         visited[r][c+1] = 1
                     } 
                     // down
                     if r < m-1 && visited[r+1][c] == 0 && grid[r+1][c] == 1 {
-                        stack = append(stack, [2]int{r+1, c})
+                        q = append(q, [2]int{r+1, c})
                         visited[r+1][c] = 1
                     }
                     // left
                     if c > 0 && visited[r][c-1] == 0 && grid[r][c-1] == 1 {
-                        stack = append(stack, [2]int{r, c-1})
+                        q = append(q, [2]int{r, c-1})
                         visited[r][c-1] = 1
                     }
                 }

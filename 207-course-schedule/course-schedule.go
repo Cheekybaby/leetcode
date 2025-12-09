28da99c724166cc94221a2898fdc3d2662a464e2
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-09 08:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.89%) - LeetSync

--- 207-course-schedule/course-schedule.go
+++ 207-course-schedule/course-schedule.go
@@ -1,24 +1,22 @@
-// Simple DFS for cycle detection
-func dfs(adj [][]int, visited []bool, inRecursion []bool, root int) bool {
+func dfs(adj [][]int, root int, visited, inRecursion []bool) bool {
     visited[root] = true
     inRecursion[root] = true
+
     for _, v := range adj[root] {
         if inRecursion[v] {
             return true
         }
-        if !visited[v] && dfs(adj, visited, inRecursion, v) {
+        if !visited[v] && dfs(adj, v, visited, inRecursion) {
             return true
         }
     }
+
     inRecursion[root] = false
     return false
 }
 
-func canFinish(numCourses int, pre [][]int) bool {
-    visited := make([]bool, numCourses)
-    inRecursion := make([]bool, numCourses)
-    adj := make([][]int, numCourses)
-
+func canFinish(n int, pre [][]int) bool {
+    adj := make([][]int, n)
     for i := range pre {
         u := pre[i][1]
         v := pre[i][0]
@@ -26,8 +24,9 @@ func canFinish(numCourses int, pre [][]int) bool {
         adj[u] = append(adj[u], v)
     }
 
-    for i := range numCourses {
-        if (visited[i] == false) && dfs(adj, visited, inRecursion, i) {
+    visited, inRecursion := make([]bool, n), make([]bool, n)
+    for i := range n {
+        if !visited[i] && dfs(adj, i, visited, inRecursion) {
             return false
         }
     }

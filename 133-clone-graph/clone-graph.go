6298ef72f1e99189b237dbbfcff0c74cfcbee758
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-09 08:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 4.8 MB (18.86%) - LeetSync

--- 133-clone-graph/clone-graph.go
+++ 133-clone-graph/clone-graph.go
@@ -11,27 +11,35 @@ func cloneGraph(node *Node) *Node {
         return node
     }
 
-    mapping := make(map[*Node]*Node)
-
-    root := &Node{node.Val, []*Node{}}
-    mapping[node] = root
+    mapping := map[*Node]*Node{}
 
     var q []*Node
     q = append(q, node)
 
-    for len(q) != 0 {
+    for len(q) > 0 {
         curr := q[0]
         q = q[1:]
 
-        for _, adj := range curr.Neighbors {
-            if _, ok := mapping[adj]; !ok {
-                mapping[adj] = &Node{adj.Val, []*Node{}}
-                q = append(q, adj)
-            }
+        newCurr := &Node{Val : curr.Val, Neighbors : []*Node{}}
+        mapping[curr] = newCurr
 
-            mapping[curr].Neighbors = append(mapping[curr].Neighbors, mapping[adj])
+        for _, v := range curr.Neighbors {
+            if _, ok := mapping[v]; !ok {
+                q = append(q, v)
+            }
         }
     }
+
+    for key, val := range mapping {
+        neigh := make([]*Node, len(key.Neighbors))
+
+        for i, v := range key.Neighbors {
+            neigh[i] = mapping[v]
+        }
+
+        val.Neighbors = neigh
+    }
+
 
     return mapping[node]
 }

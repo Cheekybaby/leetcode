75eda7087a4c94139406ce3901de3876867e807e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-10 18:50:46 +0530

    Time: 5 ms (65.56%) | Memory: 5.8 MB (63.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.go
+++ 200-number-of-islands/number-of-islands.go
@@ -1,39 +1,23 @@
-var dir = [][]int{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
-
-func bfs(grid [][]byte, i, j int) {
-    grid[i][j] = '0'
-
-    var q [][2]int
-    q = append(q, [2]int{i, j})
-
-    for len(q) > 0 {
-        curr := q[0]
-        q = q [1:]
-
-        r, c := curr[0], curr[1]
-        for _, d := range dir {
-            dr := r + d[0]
-            dc := c + d[1]
-
-            if (dr < 0 || dr >= len(grid)) || (dc < 0 || dc >= len(grid[0])) || (grid[dr][dc] == '0') {
-                continue
-            }
-
-            grid[dr][dc] = '0'
-            q = append(q, [2]int{dr, dc})
-        }
-
+func dfs(grid [][]byte, i, j int) {
+    if (i < 0 || i >= len(grid)) || (j < 0 || j >= len(grid[0])) || grid[i][j] == '0' {
+        return
     }
+
+    grid[i][j] = '0'
+    
+    dfs(grid, i+1, j)
+    dfs(grid, i-1, j)
+    dfs(grid, i, j+1)
+    dfs(grid, i, j-1)
 }
 
 func numIslands(grid [][]byte) int {
-    m, n := len(grid), len(grid[0])
     cnt := 0
-    for i := 0; i < m; i++ {
-        for j := 0; j < n; j++ {
+    for i := 0; i < len(grid); i++ {
+        for j := 0; j < len(grid[0]); j++ {
             if grid[i][j] == '1' {
                 cnt++
-                bfs(grid, i, j)
+                dfs(grid, i, j)
             }
         }
     }

d593d53cb09be957a34a9923069d16de6aa9ee4e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-12 12:55:27 +0530

    Time: 59 ms (91.15%) | Memory: 33.6 MB (99.56%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.go
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.go
@@ -1,15 +1,25 @@
-func dfs(adj [][]int, source int, destination int, visited []bool) bool {
-    if source == destination {
-        return true
-    }
-
+func bfs(adj [][]int, source int, destination int, visited []bool) bool {
     visited[source] = true
 
-    for _, v := range adj[source] {
-        if !visited[v] && dfs(adj, v, destination, visited) {
+    var q []int
+    q = append(q, source)
+
+    for len(q) > 0 {
+        node := q[0]
+        q = q[1:]
+
+        if node == destination {
             return true
         }
+
+        for _, v := range adj[node] {
+            if !visited[v] {
+                visited[v] = true
+                q = append(q, v)
+            }
+        }
     }
+
     return false
 }
 
@@ -23,10 +33,8 @@ func validPath(n int, edges [][]int, source int, destination int) bool {
         adj[u] = append(adj[u], v)
         adj[v] = append(adj[v], u)
     }
-
     visited := make([]bool, n)
-
-    path_exists := dfs(adj, source, destination, visited)
+    path_exists := bfs(adj, source, destination, visited)
 
     return path_exists
 }

130d8cc979551bc577508a4697e32d9ede7f448a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-12 18:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 3.9 MB (52.38%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.go
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.go
@@ -1,13 +1,23 @@
 func kthFactor(n int, k int) int {
     cnt := 0
-
-    for i := 1; i <= n; i++ {
+    for i := 1; i*i < n; i++ {
         if n % i == 0 {
             cnt++
         }
 
         if cnt == k {
             return i
+        }
+    }
+    x := int(math.Sqrt(float64(n)))
+
+    for i := x; i >= 1; i-- {
+        if n % i == 0 {
+            cnt++
+        }
+
+        if cnt == k {
+            return n / i
         }
     }
 

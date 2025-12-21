0945ed45f167bcd0cfb577b74011daa49da8acd6
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-21 12:52:50 +0530

    Time: 44 ms (17.14%) | Memory: 9.8 MB (11.43%) - LeetSync

--- 1572-subrectangle-queries/subrectangle-queries.go
+++ 1572-subrectangle-queries/subrectangle-queries.go
@@ -1,25 +1,44 @@
+type Update struct {
+    row1    int
+    row2    int
+    col1    int
+    col2    int
+    val     int
+}
+
 type SubrectangleQueries struct {
-    matrix [][]int
+    matrix   [][]int
+    updates  []Update
 }
 
 
 func Constructor(rectangle [][]int) SubrectangleQueries {
     return SubrectangleQueries {
-        matrix : rectangle,
+        matrix  :    rectangle,
+        updates :   make([]Update, 0),
     }
 }
 
 
 func (this *SubrectangleQueries) UpdateSubrectangle(row1 int, col1 int, row2 int, col2 int, newValue int)  {
-    for i := row1; i <= row2; i++ {
-        for j := col1; j <= col2; j++ {
-            this.matrix[i][j] = newValue
-        }
-    }
+    this.updates = append(this.updates, Update{
+        row1 : row1,
+        row2 : row2,
+        col1 : col1,
+        col2 : col2,
+        val  : newValue,
+    })
 }
 
 
 func (this *SubrectangleQueries) GetValue(row int, col int) int {
+    for i := len(this.updates) - 1; i >= 0; i-- {
+        update := this.updates[i]
+
+        if update.row1 <= row && update.row2 >= row && update.col1 <= col && update.col2 >= col {
+            return update.val
+        }
+    }
     return this.matrix[row][col]
 }
 

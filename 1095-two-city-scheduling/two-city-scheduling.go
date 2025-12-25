532c5dadeb2b2a66cdc67528988e118fbd19bcbe
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-25 19:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 4.4 MB (65.00%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.go
+++ 1095-two-city-scheduling/two-city-scheduling.go
@@ -1,35 +1,16 @@
-func solve(costs [][]int, idx int, l_a, l_b int, dp [][]int) int {
-    if idx >= len(costs) {
-        return 0
-    }
-
-    if dp[idx][l_a] != -1 {
-        return dp[idx][l_a]
-    }
-
-    var a, b int = math.MaxInt, math.MaxInt
-    if l_a < len(costs)/2 {
-        a = costs[idx][0] + solve(costs, idx+1, l_a + 1, l_b, dp)
-    }
-    if l_b < len(costs)/2 {
-        b = costs[idx][1] + solve(costs, idx+1, l_a, l_b + 1, dp)
-    }
-
-    dp[idx][l_a] = min(a, b)
-
-    return dp[idx][l_a]
-}
-
 func twoCitySchedCost(costs [][]int) int {
-    dp := make([][]int, len(costs))
-    for i := range dp {
-        dp[i] = make([]int, len(costs)/2 + 1)
-        for j := range dp[i] {
-            dp[i][j] = -1
-        }
-    }
-    
-    min_cost := solve(costs, 0, 0, 0, dp)
+    sort.Slice(costs, func (i, j int) bool {
+        diffA := costs[i][0] - costs[j][0]
+        diffB := costs[i][1] - costs[j][1]
 
-    return min_cost
+        return diffA < diffB
+    })
+    cost := 0
+    for i := 0; i < len(costs)/2; i++ {
+        cost += costs[i][0]
+    }
+    for i := len(costs)/2; i < len(costs); i++ {
+        cost += costs[i][1]
+    }
+    return cost
 }

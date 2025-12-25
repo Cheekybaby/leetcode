645e658c8f46822fc71b9b7f9df4acff45bcff9a
Shashank Ranjan <shashankranjan07@gmail.com>
2025-12-25 13:53:43 +0530

    Time: 3 ms (100.00%) | Memory: 4.8 MB (100.00%) - LeetSync

--- 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.go
+++ 2811-determine-the-minimum-sum-of-a-k-avoiding-array/determine-the-minimum-sum-of-a-k-avoiding-array.go
@@ -1,12 +1,11 @@
 func minimumSum(n int, k int) int {
     sum := 0
     cnt := 0
-    avoid := map[int]bool{}
     for i := 1; cnt < n; i += 1 {
-        if _, ok := avoid[i]; !ok {
+        cmp := k - i
+        if cmp > i || cmp <= 0 || cmp == i {
             sum += i
             cnt++
-            avoid[k - i] = true
         }
     }
 

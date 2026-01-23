2e3db41592d408e8bf324f8625a6afe42dd4bc35
Shashank Ranjan <shashankranjan07@gmail.com>
2026-01-23 19:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (87.50%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
@@ -1,36 +1,32 @@
-func solve(nums []int, visited []bool, k, idx, target, sum int) bool {
-    if k == 1 {
-        return true
-    }
-    if sum == target {
-        return solve(nums, visited, k-1, 0, target, 0)
+func solve(nums []int, idx int, res_arr []int, target int) bool {
+    if idx == len(nums) { return true }
+
+    for i := range res_arr {
+        if res_arr[i] + nums[idx] > target { continue }
+        if i > 0 && res_arr[i] == res_arr[i-1] { continue }
+        res_arr[i] += nums[idx]
+        if solve(nums, idx+1, res_arr, target) { return true }
+        res_arr[i] -= nums[idx]
     }
 
-    for j := idx; j < len(nums); j++ {
-        if visited[j] || (sum + nums[j]) > target {
-            continue
-        }
-        visited[j] = true
-        if (solve(nums, visited, k, j+1, target, sum + nums[j])) {
-            return true
-        }
-        visited[j] = false
-    }
     return false
 }
 
 func canPartitionKSubsets(nums []int, k int) bool {
-    sum := 0
-    for _, val := range nums {
-        sum += val
+    total := 0
+    for _, num := range nums {
+        total += num
     }
 
-    if (sum % k) != 0 {
+    if (total % k) != 0 {
         return false
     }
-    sort.Sort(sort.Reverse((sort.IntSlice(nums))))
-    target := (sum / k)
-    visited := make([]bool, len(nums))
-    
-    return solve(nums, visited, k, 0, target, 0)
+
+    target := total / k
+    sort.Slice(nums, func (i, j int) bool {
+        return nums[i] > nums[j]
+    })
+
+    res_arr := make([]int, k)
+    return solve(nums, 0, res_arr, target)
 }

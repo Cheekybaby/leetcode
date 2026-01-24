ef1d0d08356825ec1f5b719dcedc7e0065d32e4b
Shashank Ranjan <shashankranjan07@gmail.com>
2026-01-24 18:07:29 +0530

    Time: 9 ms (28.57%) | Memory: 8.9 MB (36.73%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.go
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.go
@@ -1,33 +1,17 @@
-func solve(s, t string, n, m int, dp [][]int) int {
-    if n < 0 || m < 0 {
-        return 0
-    }
-
-    if dp[n][m] != -1 {
-        return dp[n][m]
-    }
-
-    if s[n] == t[m] {
-        dp[n][m] = 1 + solve(s, t, n-1, m-1, dp)
-    } else {
-        dp[n][m] = max(solve(s, t, n-1, m, dp), solve(s, t, n, m-1, dp))
-    }
-    return dp[n][m]
-}
 func minDistance(word1 string, word2 string) int {
     n, m := len(word1), len(word2)
-    dp := initDP(n, m)
-    longest_subseq := solve(word1, word2, n-1, m-1, dp)
-
-    return (n + m - (2 * longest_subseq))
-}
-func initDP(n, m int) (dp [][]int) {
-    dp = make([][]int, n)
-    for i := range dp {
-        dp[i] = make([]int, m)
-        for j := range dp[i] {
-            dp[i][j] = -1
+    dp := make([]int, m+1)
+    for i := 1; i <= n; i++ {
+        temp := make([]int, m+1)
+        for j := 1; j <= m; j++ {
+            if word1[i-1] == word2[j-1] {
+                temp[j] = 1 + dp[j-1]
+            } else {
+                temp[j] = max(dp[j], temp[j-1])
+            }
         }
+        dp = temp
     }
-    return dp
+
+    return (n + m - (2 * dp[m]))
 }

48d9d33024721778b5927b620fa9ff5a5f525bee
Shashank Ranjan <shashankranjan07@gmail.com>
2026-01-24 17:49:03 +0530

    Time: 22 ms (27.15%) | Memory: 19.9 MB (34.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.go
+++ 1250-longest-common-subsequence/longest-common-subsequence.go
@@ -1,28 +1,19 @@
-func solve(s, t string, i, j int, dp [][]int) int {
-    if i >= len(s) || j >= len(t) {
-        return 0
-    }
-
-    if dp[i][j] != -1 {
-        return dp[i][j]
-    }
-
-    if s[i] == t[j] {
-        dp[i][j] = 1 + solve(s, t, i + 1, j + 1, dp)
-    } else {
-        dp[i][j] = max(solve(s, t, i + 1, j, dp), solve(s, t, i, j + 1, dp))
-    }
-    return dp[i][j]
-}
-
 func longestCommonSubsequence(text1 string, text2 string) int {
     n, m := len(text1), len(text2)
-    dp := make([][]int, n)
+    dp := make([][]int, n+1)
     for i := range dp {
-        dp[i] = make([]int, m)
-        for j := range dp[i] {
-            dp[i][j] = -1
+        dp[i] = make([]int, m+1)
+    }
+
+    for i := 1; i <= n; i++ {
+        for j := 1; j <= m; j++ {
+            if text1[i-1] == text2[j-1] {
+                dp[i][j] = 1 + dp[i-1][j-1]
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            }
         }
     }
-    return solve(text1, text2, 0, 0, dp)
+
+    return dp[n][m]
 }

b25b8e3114b9a80b4c97b5add31ca97f1052aaa6
Shashank Ranjan <shashankranjan07@gmail.com>
2026-01-24 15:06:25 +0530

    Time: 33 ms (31.02%) | Memory: 19.4 MB (47.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.go
+++ 518-coin-change-ii/coin-change-ii.go
@@ -1,47 +1,32 @@
-var dp = make([][]int, 302)
-func solve(nums []int, amount, idx int) int {
-    if idx >= len(nums) {
-        if amount == 0 {
-            return 1
-        }
-        return 0
-    }
-
-    if amount < 0 {
-        return 0
-    }
-    if amount == 0 {
-        return 1
-    }
+func solve(coins []int, amount int, idx int, dp [][]int) int {
+    if idx < 0 || amount < 0 { return 0 }
+    if amount == 0 { return 1 }
 
     if dp[idx][amount] != -1 {
         return dp[idx][amount]
     }
 
-    take := 0
-    if amount >= nums[idx] {
-        take = solve(nums, amount - nums[idx], idx)
+    take, not_take := 0, 0
+    if amount >= coins[idx] {
+        take = solve(coins, amount - coins[idx], idx, dp)
     }
-    not_take := solve(nums, amount, idx + 1)
+    not_take = solve(coins, amount, idx-1, dp)
 
-    dp[idx][amount] = (take + not_take)
-
+    dp[idx][amount] = take + not_take
     return dp[idx][amount]
 }
 
 func change(amount int, coins []int) int {
-    initDP()
-    
-    ans := solve(coins, amount, 0)
-
-    return ans
-}
-
-func initDP() {
+    n := len(coins)
+    dp := make([][]int, n)
     for i := range dp {
-        dp[i] = make([]int, 5005)
+        dp[i] = make([]int, amount + 1)
         for j := range dp[i] {
             dp[i][j] = -1
         }
     }
+
+    ways := solve(coins, amount, n-1, dp)
+
+    return ways
 }

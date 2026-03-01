4cbd436847a76fd0acdb86e4c82e524414e6c02b
Shashank Ranjan <shashankranjan07@gmail.com>
2026-03-01 10:51:33 +0530

    Time: 56 ms (25.28%) | Memory: 9.7 MB (12.51%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.go
+++ 5-longest-palindromic-substring/longest-palindromic-substring.go
@@ -1,31 +1,38 @@
 func longestPalindrome(s string) string {
-    if len(s) == 1 {
-        return s
+    isPalindrome := make([][]bool, len(s))
+    var ans string
+    for i := range isPalindrome {
+        isPalindrome[i] = make([]bool, len(s))
+    }
+    // Length 1 is always palindrome
+    for i := range len(s) {
+        isPalindrome[i][i] = true
+        ans = s[i:i+1]
+    }
+    // Length 2
+    for i := 0; i < len(s)-1; i++ {
+        if s[i] == s[i+1] { 
+            isPalindrome[i][i+1] = true
+            isPalindrome[i+1][i] = true
+            ans = s[i:i+2]
+        }
+    }
+    // For length 3 and beyond
+    for k := 3; k <= len(s); k++ {
+        l, r := 0, k-1
+        for r < len(s) {
+            if s[l] == s[r] && isPalindrome[l+1][r-1] {
+                isPalindrome[l][r] = true
+                isPalindrome[r][l] = true
+
+                ans = s[l:r+1]
+            }
+
+            l++
+            r++
+        }
     }
 
-    st, end := 0, 0
-	for i := range s {
-        len1 := expand(s, i, i)
-        len2 := expand(s, i, i+1)
-
-        max_len := max(len1, len2)
-
-		if max_len > end - st {
-            st = i - (max_len - 1)/2
-            end = i + (max_len)/2
-        }
-	}
-
-	return s[st:end+1]
-}
-
-func expand(s string, st, end int) int {
-	l, r := st, end
-
-	for l >= 0 && r < len(s) && s[l] == s[r] {
-		l--
-		r++
-	}
-
-    return r - l - 1
+    // fmt.Println(isPalindrome)
+    return ans
 }

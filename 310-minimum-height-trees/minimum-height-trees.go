48ae74d46905ec98ab74722428fbe70de9ac610d
Shashank Ranjan <shashankranjan07@gmail.com>
2026-03-07 23:59:02 +0530

    Time: 14 ms (88.70%) | Memory: 9.6 MB (90.43%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.go
+++ 310-minimum-height-trees/minimum-height-trees.go
@@ -1,54 +1,37 @@
 func findMinHeightTrees(n int, edges [][]int) []int {
-    if n == 1 {
-        return []int{0}
-    }
-
+    if n == 1 { return []int{0} }
     adj := make([][]int, n)
-
-    indegree := make([]int, n)
-
-    for i := range edges {
-        u := edges[i][0]
-        v := edges[i][1]
+    degree := make([]int, n)
+    for _, edge := range edges {
+        u := edge[0]
+        v := edge[1]
 
         adj[u] = append(adj[u], v)
         adj[v] = append(adj[v], u)
 
-        indegree[u]++
-        indegree[v]++
+        degree[u]++
+        degree[v]++
     }
 
-    var q []int
-    for i := range indegree {
-        if indegree[i] == 1 {
-            q = append(q, i)
-        }
+    var leaves []int
+    for i := range adj {
+        if degree[i] == 1 { leaves = append(leaves, i) }
     }
 
-    remaining := n
-    for remaining > 2 {
-        size := len(q)
-        remaining -= size
+    for n > 2 {
+        n -= len(leaves)
+        var newLeaves []int
 
-        for _ = range size {
-            node := q[0]
-            q = q[1:]
+        for _, leaf := range leaves {
+            for _, nei := range adj[leaf] {
+                degree[nei]--
 
-            for _, v := range adj[node] {
-                indegree[v]--
-
-                if indegree[v] == 1 {
-                    q = append(q, v)
-                }
+                if degree[nei] == 1 { newLeaves = append(newLeaves, nei) }
             }
         }
+
+        leaves = newLeaves
     }
 
-    var root []int
-    for len(q) > 0 {
-        root = append(root, q[0])
-        q = q[1:]
-    }
-
-    return root
+    return leaves
 }

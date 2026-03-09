12ddd302033e8d6d72b984b13311697a0e8df98e
Shashank Ranjan <shashankranjan07@gmail.com>
2026-03-09 21:55:55 +0530

    Time: 3 ms (44.49%) | Memory: 9 MB (5.15%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.go
+++ 210-course-schedule-ii/course-schedule-ii.go
@@ -1,40 +1,59 @@
-func bfs(adj [][]int, indegree []int) (order []int) {
-    var q []int
-    for i, val := range indegree {
-        if val == 0 { q = append(q, i) }
-    }
+func dfs(adj [][]int, root int, visited, inRecursion []bool) (order []int, hasLoop bool) {
+    visited[root] = true
+    inRecursion[root] = true
 
-    for len(q) > 0 {
-        curr := q[0]
-        q = q[1:]
-
-        order = append(order, curr)
-
-        for _, v := range adj[curr] {
-            indegree[v]--
-
-            if indegree[v] == 0 { q = append(q, v) }
+    for _, v := range adj[root] {
+        if inRecursion[v] {
+            return []int{}, true
+        }
+        if !visited[v] {
+            val, loop := dfs(adj, v, visited, inRecursion)
+            if loop { return []int{}, true }
+            order = append(order, val...)
         }
     }
+    order = append(order, root)
 
-    return order
+    inRecursion[root] = false
+    return order, false
 }
 
 func findOrder(numCourses int, prerequisites [][]int) []int {
     adj := make([][]int, numCourses)
-    indegree := make([]int, numCourses)
     for _, course := range prerequisites {
         u := course[1]
         v := course[0]
 
         adj[u] = append(adj[u], v)
-        indegree[v]++
     }
 
-    order := bfs(adj, indegree)
+    visited, inRecursion := make([]bool, numCourses), make([]bool, numCourses)
+    var finalOrder []int
+    for i := range adj {
+        if !visited[i] {
+            order, hasLoop := dfs(adj, i, visited, inRecursion)
+            
+            if hasLoop {
+                return order
+            }
+            
+            finalOrder = append(finalOrder, order...)
+        }
+    }
 
-    if len(order) != numCourses {
+    if len(finalOrder) != numCourses {
         return []int{}
     }
-    return order
+    reverse(finalOrder)
+
+    return finalOrder
+}
+
+func reverse(nums []int) {
+    i, j := 0, len(nums)-1
+    for i < j {
+        nums[i], nums[j] = nums[j], nums[i]
+        i++
+        j--
+    }
 }

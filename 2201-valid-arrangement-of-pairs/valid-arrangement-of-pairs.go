414ac085a833c160538cade8308c66b354dff0c2
Shashank Ranjan <shashankranjan07@gmail.com>
2026-03-10 11:31:19 +0530

    Time: 234 ms (70.83%) | Memory: 39.2 MB (66.67%) - LeetSync

--- 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.go
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.go
@@ -1,58 +1,69 @@
-func dfs(adj map[int][]int, start int, path *[]int) {
+type Degree struct {
+    IN  int
+    OUT int
+}
+
+func dfs(adj map[int][]int, root int) (order []int) {
     var st []int
-    st = append(st, start)
+    st = append(st, root)
 
     for len(st) > 0 {
         node := st[len(st)-1]
 
         if len(adj[node]) > 0 {
-            neighbor := adj[node][len(adj[node])-1]
-            st = append(st, neighbor)
-            adj[node] = adj[node][:len(adj[node])-1]
+            v := adj[node][0]
+            st = append(st, v)
+            adj[node] = adj[node][1:]
         } else {
-            *path = append(*path, node)
+            order = append(order, node)
             st = st[:len(st)-1]
         }
     }
+
+    return order
+}
+
+func makePairs(nums []int) (pairs [][]int) {
+    i := len(nums)-1
+    for i > 0 {
+        pair := []int{nums[i], nums[i-1]}
+        pairs = append(pairs, pair)
+        i--
+    }
+    return pairs
 }
 
 func validArrangement(pairs [][]int) [][]int {
     adj := map[int][]int{}
-    degree := map[int][2]int{} // [0], [1] = indegree, outdegree
+    degree := map[int]Degree{}
     for _, pair := range pairs {
         u := pair[0]
         v := pair[1]
 
         adj[u] = append(adj[u], v)
 
-        var temp [2]int
-        temp = degree[u]
-        temp[1]++
-        degree[u] = temp
+        du := degree[u]
+        du.OUT++
+        degree[u] = du
 
-        temp = degree[v]
-        temp[0]++
-        degree[v] = temp
+        dv := degree[v]
+        dv.IN++
+        degree[v] = dv
     }
-    // find the starting node
+
     var root int = pairs[0][0]
     for key, val := range degree {
-        if val[1] - val[0] == 1 {
+        in := val.IN
+        out := val.OUT
+
+        if (out - in) == 1 {
             root = key
             break
         }
     }
 
-    // hit a DFS over it
-    var path []int
-    dfs(adj, root, &path)
+    order := dfs(adj, root)
+    validPairs := makePairs(order)
 
-    // create the final result
-    var res [][]int
-    for i := len(path)-1; i > 0; i-- {
-        pair := []int{path[i], path[i-1]}
-        res = append(res, pair)
-    }
-
-    return res
+    return validPairs
 }

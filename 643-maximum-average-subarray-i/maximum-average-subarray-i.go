4aec356b2ff891a9678c0d82e18ac20eb3db95bc
Shashank Ranjan <shashankranjan07@gmail.com>
2026-03-24 10:35:12 +0530

    Time: 4 ms (15.91%) | Memory: 9.7 MB (66.32%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.go
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.go
@@ -1,16 +1,22 @@
 func findMaxAverage(nums []int, k int) float64 {
-    maxAvg := -math.MaxFloat64
-    j, sum := 0, 0
-    for i:=0; i<len(nums); i++ {
-        length := i - j + 1
+    var max_avg float64 = -math.MaxFloat64
+    var sum int
+    j := 0
+    for i := 0; i < len(nums); i++ {
         sum += nums[i]
-        if length == k {
-            avg := float64(sum) / float64(k)
-            maxAvg = max(maxAvg, avg)
+        win_len := i - j + 1
+        
+        for win_len > k {
             sum -= nums[j]
-            j+=1
+            j++
+            win_len = i - j + 1
+        }
+
+        if win_len == k {
+            avg := float64(sum) / float64(k)
+            max_avg = max(max_avg, avg)
         }
     }
 
-    return maxAvg
+    return max_avg
 }
